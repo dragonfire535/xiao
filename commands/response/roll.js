d5e0fe33195118acd4bdace8ff6fb84d0e0eef49
Daniel Odendahl <danielbodendahl@gmail.com>
2017-02-27 22:23:48 -0500

    Fixes in Roll Command

--- commands/response/roll.js
+++ commands/response/roll.js
@@ -16,12 +16,12 @@ class RollChooseCommand extends commando.Command {
             if(!message.channel.permissionsFor(this.client.user).hasPermission('SEND_MESSAGES')) return;
             if(!message.channel.permissionsFor(this.client.user).hasPermission('READ_MESSAGES')) return;
         }
-        if(message.content.match(/\d/)) {
-            let [value] = message.content.split(" ").slice(1);
+        let [value] = message.content.split(" ").slice(1);
+        if(value.match("^[0-9]+$")) {
             let roll = Math.floor(Math.random() * value) + 1;
             message.reply("You rolled a " + roll);
         } else {
-            message.reply(":x: This message contains no number!");
+            message.reply(":x: Error! Your message either contains no number, the number is invalid, or the number is in the wrong place.\n:notepad_spiral: (Note: When using numbers such as 1,000, do not use a comma)");
         }
     }
 }
