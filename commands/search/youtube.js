ba6325eda8b20ec50350af3325be6bc81ff0eb34
dragonfire535 <danielbodendahl@gmail.com>
2017-03-11 16:09:24 -0500

    Permissions Check Fix in YouTube

--- commands/search/youtube.js
+++ commands/search/youtube.js
@@ -18,6 +18,7 @@ class YouTubeCommand extends commando.Command {
         if(message.channel.type !== 'dm') {
             if(!message.channel.permissionsFor(this.client.user).hasPermission('SEND_MESSAGES')) return;
             if(!message.channel.permissionsFor(this.client.user).hasPermission('READ_MESSAGES')) return;
+            if(!message.channel.permissionsFor(this.client.user).hasPermission('EMBED_LINKS')) return;
         }
         console.log("[Command] " + message.content);
         let videotosearch = message.content.split(" ").slice(1).join("-");
