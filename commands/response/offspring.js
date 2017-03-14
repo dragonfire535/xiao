b3d61772e6f500841b02b3ba0a17fb7303a954f7
dragonfire535 <danielbodendahl@gmail.com>
2017-03-13 21:20:52 -0400

    Offspring Command

---
+++ commands/response/offspring.js
@@ -0,0 +1,25 @@
+const commando = require('discord.js-commando');
+
+class OffspringCommand extends commando.Command {
+    constructor(Client){
+        super(Client, {
+            name: 'offspring', 
+            group: 'response',
+            memberName: 'offspring',
+            description: 'Tells you if your new child is a boy or a girl. (;offspring)',
+            examples: [';offspring']
+        });
+    }
+
+    async run(message, args) {
+        if(message.channel.type !== 'dm') {
+            if(!message.channel.permissionsFor(this.client.user).hasPermission('SEND_MESSAGES')) return;
+            if(!message.channel.permissionsFor(this.client.user).hasPermission('READ_MESSAGES')) return;
+        }
+        console.log("[Command] " + message.content);
+        let gender = ['boy', 'girl'][Math.floor(Math.random() * 2)];
+        message.channel.sendMessage("It's a " + gender + "!");
+    }
+}
+
+module.exports = OffspringCommand;
