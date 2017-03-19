707a942108cfd6fc8fbab7cd930a307a9e7889cd
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-03-19 03:01:16 +0000

    Attempt Soundboard

---
+++ commands/random/soundboard.js
@@ -0,0 +1,50 @@
+const commando = require('discord.js-commando');
+const sounds = require('./sounds.json');
+
+class SoundBoardCommand extends commando.Command {
+    constructor(Client){
+        super(Client, {
+            name: 'soundboard', 
+            group: 'random',
+            memberName: 'soundboard',
+            description: 'Plays a sound in your voice channel. (;soundboard cat)',
+            examples: [';soundboard cat']
+        });
+    }
+
+    async run(message, args) {
+        if(message.channel.type !== 'dm') {
+            if(!message.channel.permissionsFor(this.client.user).hasPermission('SEND_MESSAGES')) return;
+            if(!message.channel.permissionsFor(this.client.user).hasPermission('READ_MESSAGES')) return;
+        }
+        console.log("[Command] " + message.content);
+        if(message.channel.type !== 'dm') {
+            if(!message.channel.permissionsFor(this.client.user).hasPermission('CONNECT' || 'SPEAK')) {
+                message.channel.send(':x: Error! In order to do this command, you must give me the permissions to "Connect" and "Speak"!');
+            } else {
+                let voiceChannel = message.member.voiceChannel;
+                if (!voiceChannel) {
+	                return message.channel.send(`:x: Error! Please be in a voice channel first!`);
+                }
+                voiceChannel.join().then(connnection => {
+	                let soundToPlay = message.content.toLowerCase().split(" ").slice(1).join(" ");
+	                if(soundToPlay === "") {
+		                message.channel.send(':x: Error! No sound set. Please use ;soundboard list to see a list of sounds you can play.');
+	                } else if(soundToPlay === 'list') {
+		                message.channel.send('**Avaliable Sounds:** Meow');
+	                } else if(soundToPlay === sounds.avaliable[soundToPlay]) {
+		                let stream = sounds.paths[soundToPlay];
+    	                let dispatcher = connnection.playStream(stream);
+    	                dispatcher.on('end', () => {
+        	                voiceChannel.leave();
+    	                });
+	                } else {
+		                message.channel.send(':x: Error! Sound not found! Please use ;soundboard list to see a list of sounds you can play.');
+	                }
+                });         
+            }
+        }
+    }
+}
+
+module.exports = SoundBoardCommand;
