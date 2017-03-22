f49d87e63441e2199709ea2b5a78ff53b04e6c17
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-03-22 00:37:11 +0000

    Choose Changes

--- commands/response/choose.js
+++ commands/response/choose.js
@@ -19,12 +19,13 @@ module.exports = class ChooseCommand extends commando.Command {
             if(!message.channel.permissionsFor(this.client.user).hasPermission(['SEND_MESSAGES', 'READ_MESSAGES'])) return;
         }
         console.log("[Command] " + message.content);
-        if(message.content.includes("|")) {
-            let choice2 = message.content.split("|").slice(1).join(" ");
-            let choice1 = " " + message.content.replace(choice2, "").split(" ").slice(1).join(" ");
-            let coin = [choice1, choice2];
-            coin = coin[Math.floor(Math.random() * coin.length)];
-            message.channel.send("I choose" + coin.replace("|", ""));
+        if(message.content.includes(" | ")) {
+            let choices = message.content.split(" | ");
+            let choice1 = choices[0];
+            let choice2 = choices[1];
+            let randomChoice = [choice1, choice2];
+            randomChoice = randomChoice[Math.floor(Math.random() * randomChoice.length)];
+            message.channel.send("I choose " + randomChoice);
         } else {
             message.channel.send(":x: Split your two choices with a ' | '!");
         }
