e88ec8362ed0a10bd9c0900d040c2c3b8478ce39
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-04-08 03:26:04 +0000

    Add a Few Stragglers

--- commands/guildinfo/guildinfo.js
+++ commands/guildinfo/guildinfo.js
@@ -22,7 +22,8 @@ module.exports = class GuildInfoCommand extends commando.Command {
 
     run(message) {
         if (message.channel.type !== 'dm') {
-            if (!message.channel.permissionsFor(this.client.user).hasPermission(['SEND_MESSAGES', 'READ_MESSAGES', 'EMBED_LINKS'])) return;
+            if (!message.channel.permissionsFor(this.client.user).hasPermission(['SEND_MESSAGES', 'READ_MESSAGES'])) return;
+            if (!message.channel.permissionsFor(this.client.user).hasPermission('EMBED_LINKS')) return message.say(':x: Error! I don\'t have the Embed Links Permission!');
         }
         console.log(`[Command] ${message.content}`);
         const embed = new Discord.RichEmbed()
