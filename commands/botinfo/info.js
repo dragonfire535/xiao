f62f6babb76cd7e09fa79a0a01e62d1bc661f1b6
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-04-11 12:18:01 +0000

    Seperate Info and Shard Info Commands

--- commands/botinfo/info.js
+++ commands/botinfo/info.js
@@ -14,13 +14,8 @@ module.exports = class InfoCommand extends Command {
             ],
             group: 'botinfo',
             memberName: 'info',
-            description: 'Gives some bot info. (;info)',
-            examples: [';info'],
-            args: [{
-                key: 'shardID',
-                prompt: 'Which Shard would you like to get data for? You can find your shard with `;server`.',
-                type: 'integer'
-            }]
+            description: 'Gives some bot info for your shard. (;info)',
+            examples: [';info']
         });
     }
 
@@ -29,30 +24,24 @@ module.exports = class InfoCommand extends Command {
             if (!message.channel.permissionsFor(this.client.user).hasPermission(['SEND_MESSAGES', 'READ_MESSAGES'])) return;
             if (!message.channel.permissionsFor(this.client.user).hasPermission('EMBED_LINKS')) return message.say(':x: Error! I don\'t have the Embed Links Permission!');
         }
-        const shardID = args.shardID;
-        if (shardID > this.client.options.shardCount - 1 || shardID < 0) {
-            return message.say(':x: Error! Invalid Shard!');
-        }
-        const memory = await this.client.shard.broadcastEval('Math.round(process.memoryUsage().heapUsed / 1024 / 1024)');
-        const uptime = await this.client.shard.fetchClientValues('uptime');
         const guilds = await this.client.shard.fetchClientValues('guilds.size');
         const embed = new RichEmbed()
             .setColor(0x00AE86)
             .setFooter(`©2017 dragonfire535 | Version ${version} | Created ${moment.duration(Date.now() - this.client.user.createdTimestamp).format('y[ years], M[ months], w[ weeks, and ]d[ days]')} ago!`)
             .addField('Servers',
-                `${guilds[shardID]} / ${guilds.reduce((prev, val) => prev + val, 0)}`, true)
+                `${this.client.guilds.size} / ${guilds.reduce((prev, val) => prev + val, 0)}`, true)
             .addField('Shards',
-                `${this.client.options.shardCount} (${shardID})`, true)
+                `${this.client.options.shardCount} (${this.client.shard.id})`, true)
             .addField('Commands',
-                '113', true)
+                '114', true)
             .addField('Owner',
                 'dragonfire535#8081', true)
             .addField('Source Code',
                 '[View Here](https://github.com/dragonfire535/xiaobot)', true)
             .addField('Memory Usage',
-                `${memory[shardID]}MB`, true)
+                `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`, true)
             .addField('Uptime',
-                moment.duration(uptime[shardID]).format('d[d]h[h]m[m]s[s]'), true)
+                moment.duration(this.client.uptime).format('d[d]h[h]m[m]s[s]'), true)
             .addField('Node Version',
                 process.version, true)
             .addField('Library',
