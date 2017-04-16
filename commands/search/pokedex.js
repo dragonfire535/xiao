b9e8b8660a525ba8503e7f8c0dfc1c672e15b9bd
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-04-16 16:51:10 +0000

    Whoops

--- commands/search/pokedex.js
+++ commands/search/pokedex.js
@@ -22,7 +22,7 @@ module.exports = class PokedexCommand extends Command {
         });
     }
 
-    run(message, args) {
+    async run(message, args) {
         if (message.channel.type !== 'dm') {
             if (!message.channel.permissionsFor(this.client.user).hasPermission(['SEND_MESSAGES', 'READ_MESSAGES'])) return;
             if (!message.channel.permissionsFor(this.client.user).hasPermission('EMBED_LINKS')) return message.say(':x: Error! I don\'t have the Embed Links Permission!');
