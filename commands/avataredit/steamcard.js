2d108cc78a9b30f139e1d6024654fae0c2706dec
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-04-22 14:40:49 +0000

    No Need for Blank Images with this

--- commands/avataredit/steamcard.js
+++ commands/avataredit/steamcard.js
@@ -27,17 +27,17 @@ module.exports = class SteamCardCommand extends Command {
         }
         const { user } = args;
         const userAvatar = user.displayAvatarURL.replace('.jpg', '.png').replace('.gif', '.png');
+        const blank = new Jimp(494, 568, 0xFFFFFF);
         let images = [];
         images.push(Jimp.read(userAvatar));
         images.push(Jimp.read('./images/SteamCard.png'));
-        images.push(Jimp.read('./images/SteamCardBlank.png'));
-        const [avatar, steamcard, nothing] = await Promise.all(images);
+        const [avatar, steamcard] = await Promise.all(images);
         const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
         avatar.resize(450, 450);
-        nothing.composite(avatar, 25, 25);
-        nothing.composite(steamcard, 0, 0);
-        nothing.print(font, 38, 20, user.username);
-        nothing.getBuffer(Jimp.MIME_PNG, (err, buff) => {
+        blank.composite(avatar, 25, 25);
+        blank.composite(steamcard, 0, 0);
+        blank.print(font, 38, 20, user.username);
+        blank.getBuffer(Jimp.MIME_PNG, (err, buff) => {
             if (err) return message.say(':x: Error! Something went wrong!');
             return message.channel.send({files: [{attachment: buff}]});
         });
