37c9eb32ef3ac8d0f02659b7a46da8d32869ba9d
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-04-23 02:23:49 +0000

    LMGTFY Command

---
+++ commands/search/lmgtfy.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class LMGTFYCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'lmgtfy',
+            group: 'search',
+            memberName: 'lmgtfy',
+            description: 'Responds with a LMGTFY link. (x;lmgtfy How to Internet)',
+            examples: ['x;lmgtfy How to Internet'],
+            args: [{
+                key: 'query',
+                prompt: 'What would you like to the link to search for?',
+                type: 'string',
+                parse: text => {
+                    return encodeURIComponent(text);
+                }
+            }]
+        });
+    }
+
+    run(message, args) {
+        if (message.channel.type !== 'dm') {
+            if (!message.channel.permissionsFor(this.client.user).hasPermission(['SEND_MESSAGES', 'READ_MESSAGES'])) return;
+        }
+        const { query } = args;
+        return message.say(`http://lmgtfy.com/?iie=1&q=${query}`);
+    }
+};
