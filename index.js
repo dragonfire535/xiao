e98055353901e6fe182f20f2d8f143816514b23e
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-04-26 15:14:11 +0000

    Fix Mentioning

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ client.registry
 client.on('guildMemberAdd', (member) => {
     const channel = member.guild.channels.find('name', 'member_logs');
     if (!channel) return;
-    channel.send(`Welcome ${member}!`);
+    channel.send(`Welcome ${member.user.username}!`);
 });
 
 client.on('guildMemberRemove', (member) => {
     const channel = member.guild.channels.find('name', 'member_logs');
     if (!channel) return;
-    channel.send(`Bye ${member}...`);
+    channel.send(`Bye ${member.user.username}...`);
 });
 
 client.on('guildCreate', async(guild) => {
