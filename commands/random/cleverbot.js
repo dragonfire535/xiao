1d794d9bf7a79ef317c88f6c08ae9af0f5f3ac6b
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-05-06 05:09:28 +0000

    ???

--- commands/random/cleverbot.js
+++ commands/random/cleverbot.js
@@ -29,6 +29,7 @@ module.exports = class CleverbotCommand extends Command {
             const { body } = await request
                 .post('https://cleverbot.io/1.0/ask')
                 .send({ user: process.env.CLEVS_USER, key: process.env.CLEVS_KEY, text });
+            console.log(body.status, body.response, body);
             if(body.status !== 'success') throw new Error(body.status);
             return msg.reply(body.response);
         } catch(err) {
