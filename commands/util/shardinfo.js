ff30e857bf067c9044f45925ab07bab89684a29b
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-05-07 17:43:17 +0000

    Oops

--- commands/util/shardinfo.js
+++ commands/util/shardinfo.js
@@ -19,10 +19,7 @@ module.exports = class ShardInfoCommand extends Command {
                     prompt: 'Which Shard would you like to get data for?',
                     type: 'integer',
                     validate: shard => {
-                        if(shard < this.client.options.shardCount || shard > -1) {
-                            console.log(shard, this.client.options.shardCount);
-                            return true;
-                        }
+                        if(shard < this.client.options.shardCount && shard > -1) return true;
                         return 'Invalid Shard ID';
                     }
                 }
