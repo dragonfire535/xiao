0ba8311ad6af30ae2476b1984e9354ef720332dd
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-05-09 01:59:39 +0000

    Optional Konachan Query

--- commands/randomimg/konachan.js
+++ commands/randomimg/konachan.js
@@ -7,19 +7,28 @@ module.exports = class KonachanCommand extends Command {
             name: 'konachan',
             group: 'randomimg',
             memberName: 'konachan',
-            description: 'Sends a random (Possibly NSFW!) anime image from Konachan.',
-            guildOnly: true
+            description: 'Sends a random (Possibly NSFW!) anime image from Konachan, with optional query.',
+            guildOnly: true,
+            args: [
+                {
+                    key: 'query',
+                    prompt: 'What would you like to search for?',
+                    type: 'string',
+                    default: ''
+                }
+            ]
         });
     }
 
-    async run(msg) {
+    async run(msg, args) {
         if (!msg.channel.nsfw) return msg.say('This Command can only be used in NSFW Channels.');
         if (!msg.channel.permissionsFor(this.client.user).has('ATTACH_FILES'))
             return msg.say('This Command requires the `Attach Files` Permission.');
+        const { query } = args;
         try {
             const { body } = await request
-                .get('https://konachan.net/post.json?tags=order:random&limit=1');
-            return msg.channel.send({ files: [`https:${body[0].file_url}`] })
+                .get(`https://konachan.net/post.json?tags=${query ? `${query}%20` : ''}order:random&limit=1`);
+            return msg.channel.send(query ? `Result for ${query}:` : 'Random Image:', { files: [`https:${body[0].file_url}`] })
                 .catch(err => msg.say(err));
         } catch (err) {
             return msg.say(err);
