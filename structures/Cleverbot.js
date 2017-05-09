a1bae1b33dbdb6418aae2069c4f827a842cd9408
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-05-09 18:21:44 +0000

    Stop Killing my spacing git

--- structures/Cleverbot.js
+++ structures/Cleverbot.js
@@ -1,31 +1,31 @@
 const request = require('superagent');
 
 class Cleverbot {
-    constructor(options) {
-        this.key = options.key;
-        this.user = options.user;
-        this.nick = options.nick;
-    }
-    
-	create() {
-	    return request
-	        .post('https://cleverbot.io/1.0/create')
-	        .send({
-	            user: this.user,
-	            key: this.key,
-	            nick: this.nick
-	        });
+	constructor(options) {
+		this.key = options.key;
+		this.user = options.user;
+		this.nick = options.nick;
 	}
-    
+	
+	create() {
+		return request
+			.post('https://cleverbot.io/1.0/create')
+			.send({
+				user: this.user,
+				key: this.key,
+				nick: this.nick
+			});
+	}
+
 	ask(text) {
-	    return request
-	        .post('https://cleverbot.io/1.0/ask')
-	        .send({
-	            user: this.user,
-	            key: this.key,
-	            nick: this.nick,
-	            text
-	        });
+		return request
+			.post('https://cleverbot.io/1.0/ask')
+			.send({
+				user: this.user,
+				key: this.key,
+				nick: this.nick,
+				text
+			});
 	}
 }
 
