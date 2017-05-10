a67274d8cf0c7d28422ec1941c7c635231a84e74
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-05-10 00:56:13 +0000

    Prettier carbon and dbots

--- structures/Stats.js
+++ structures/Stats.js
@@ -1,18 +1,22 @@
 const request = require('superagent');
-const { CARBON_KEY, DISCORD_BOTS_KEY } = process.env;
+const { CARBON_KEY, DBOTS_KEY } = process.env;
 
 class Stats {
-    static discordBots(server_count, userID) {
-        return request
-            .post(`https://bots.discord.pw/api/bots/${userID}/stats`)
-            .set({ 'Authorization': DISCORD_BOTS_KEY })
-            .send({ server_count });
+    static dBots(server_count, id) {
+        request
+            .post(`https://bots.discord.pw/api/bots/${id}/stats`)
+            .set({ 'Authorization': DBOTS_KEY })
+            .send({ server_count })
+            .then(() => console.log('[Carbon] Successfully posted to Carbon.'))
+            .catch(err => console.error(`[Carbon] Failed to post to Carbon. ${err}`));
     }
-    
+
     static carbon(servercount) {
-        return request
+        request
             .post('https://www.carbonitex.net/discord/data/botdata.php')
-            .send({ key: CARBON_KEY, servercount });
+            .send({ key: CARBON_KEY, servercount })
+            .then(() => console.log('[DBots] Successfully posted to DBots.'))
+            .catch(err => console.error(`[DBots] Failed to post to DBots. ${err}`));
     }
 }
 
