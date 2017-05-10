b2363ba08f84822aa0127bc9acd50157f7346841
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-05-10 05:56:16 +0000

    Meow

--- commands/avataredit/steamcard.js
+++ commands/avataredit/steamcard.js
@@ -43,7 +43,7 @@ module.exports = class SteamCardCommand extends Command {
                 ctx.drawImage(base, 0, 0);
                 ctx.font = '32px Open Sans';
 			    ctx.fillStyle = 'white';
-			    ctx.fillText(username, 38, 20);
+			    ctx.fillText(username, 28, 53);
             };
             const cardImg = await request
                 .get('https://i.imgur.com/JF0WwQX.png');
