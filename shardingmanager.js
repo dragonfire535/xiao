57ec292dee05370b2598aac6880ba31f33dd6975
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-05-13 21:24:19 +0000

    2 Shards Only

--- shardingmanager.js
+++ shardingmanager.js
@@ -1,4 +1,4 @@
 const { ShardingManager } = require('discord.js');
 const { TOKEN } = process.env;
 const Manager = new ShardingManager('./index.js', { token: TOKEN });
-Manager.spawn();
+Manager.spawn(2);
