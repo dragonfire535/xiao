bad955d263836ca9308de4b82a068e8816fcf22b
Ruyi Li <ruyili2002@gmail.com>
2017-06-01 17:36:43 -0400

    Update covfefify.js

--- commands/textedit/covfefify.js
+++ commands/textedit/covfefify.js
@@ -3,40 +3,36 @@ const { wordTrans } = require('custom-translate');
 const dictionary = require('../../assets/json/covfefe');
 
 module.exports = class CovfefifyCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'covfefify',
-			group: 'textedit',
-			memberName: 'covfefify',
-			description: 'Covfefify text.',
-			args: [
+    constructor(client) {
+        super(client, {
+            name: 'covfefify',
+            group: 'textedit',
+            memberName: 'covfefify',
+            description: 'Covfefify text.',
+            args: [
 				{
-                	key: 'text',
-                	prompt: 'What text would you like to covfefify?',
-                	type: 'string',
-                	validate: text => {
-                		if (text.length < 1999) return true;
-                		return 'Your text is too long.';
-                	},
-                	parse: text => {
-                    let test = text;
-                    let firstVowel = test.match(/[aeiouy]/i).index;
-                    let tempTest = test.substring(firstVowel, test.length);
-                    let afterConsonant = tempTest.match(/[qwrtpsdfghjklzxcvbnm]/i);
-                    let cut = test.substring(afterConsonant.index + firstVowel + 1, test.length).match(/[aeiouy]/i)[0];
-                    test = test.substring(0, afterConsonant.index + firstVowel + 1);
-                    let last = test.match(/[qwrtpsdfghjklzxcvbnm]/g).pop();
-                    let swapped = wordTrans(last, dictionary);
-                    let res = test + (swapped + cut).repeat(2);
-                    return res;
-                  }
+					key: 'text',
+					prompt: 'What text would you like to covfefify?',
+					type: 'string',
+					validate: text => {
+						if (text.length < 1999) return true;
+						return 'Your text is too long.';
+					}
 				}
-            ]
-		});
-	}
+			]
+        });
+    }
 
-	run(msg, args) {
-		const { text } = args;
-		return msg.say(`\u180E${text}`);
-	}
+    run(msg, args) {
+        let { text } = args;
+        const firstVowel = text.match(/[aeiouy]/i).index;
+        let tempTest = text.substring(firstVowel, text.length);
+        let afterConsonant = tempTest.match(/[qwrtpsdfghjklzxcvbnm]/i);
+        const cut = text.substring(afterConsonant.index + firstVowel + 1, test.length).match(/[aeiouy]/i)[0];
+        text = text.substring(0, afterConsonant.index + firstVowel + 1);
+        const last = text.match(/[qwrtpsdfghjklzxcvbnm]/g).pop();
+        const swapped = wordTrans(last, dictionary);
+        const res = text + (swapped + cut).repeat(2);
+        return msg.say(`\u180E${text}`);
+    }
 };
