23dfe24b7f39baafe4ff9da33fb7a95c0615a1e1
Ruyi Li <ruyili2002@gmail.com>
2017-06-03 21:38:46 -0400

    Update covfefify.js

--- commands/textedit/covfefify.js
+++ commands/textedit/covfefify.js
@@ -23,25 +23,26 @@ module.exports = class CovfefifyCommand extends Command {
         });
     }
 
-    run(msg, args) {
+    async run(msg, args) {
         const { text } = args;
         const converted = this.covfefify(text);
         return msg.say(`\u180E${converted}`);
     }
 
-    covfefify(text) {
+    async covfefify(text) {
         text = text.toLowerCase();
-        let words = text.split(' ');
-        for(let i = 0; i < words.length; i++){
+        const words = text.split(' ');
+        for (let i = 0; i < words.length; i++){
             const firstVowel = words[i].match(/[aeiouy]/).index;
             const tempTest = words[i].substring(firstVowel, text.length);
             const afterConsonant = tempTest.match(/[qwrtpsdfghjklzxcvbnm]/);
-            const cut = words[i].substring(afterConsonant.index + firstVowel + 1, words[i].length).match(/[aeiouy]/i)[0];
+            const cutIndex = afterConsonant.index + firstVowel + 1;
+            const cut = words[i].substring(cutIndex, words[i].length).match(/[aeiouy]/i)[0];
             words[i] = words[i].substring(0, afterConsonant.index + firstVowel + 1);
             const last = words[i].match(/[qwrtpsdfghjklzxcvbnm]/g).pop();
             const swapped = letterTrans(last, dictionary);
-            words[i] = words[i] + (swapped + cut).repeat(2);
+            words[i] = words[i] + swapped + cut + swapped + cut;
         }
-        return words.join(' ')
+        return words.join(' ');
     }
 };
