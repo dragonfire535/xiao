29a1c9b9475984017f22e85ccf5fc4dc3770cb42
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-06-24 21:14:12 +0000

    Allow modlog channel to be swapped

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -33,7 +33,11 @@ module.exports = class BanCommand extends Command {
     }
 
     async run(msg, args) {
-        const modlogs = msg.guild.channels.find('name', 'mod-log');
+        const modlogs = msg.guild.channels.filter((c) => {
+            const topic = c.topic || '';
+            if (topic.includes('<modlog>')) return true;
+            else return false;
+        }).first() || member.guild.channels.find('name', 'mod-log');
         const { member, reason } = args;
         if (!member.bannable) return msg.say('This member is not bannable. Perhaps they have a higher role than me?');
         await msg.say(`Are you sure you want to ban ${member.user.tag} (${member.id})?`);
