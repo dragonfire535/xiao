2edc0c0fbdf215804ca97711c37b88d1b08bc2cf
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-07-11 18:12:26 +0000

    Try/Catch Recipe

--- commands/search/recipe.js
+++ commands/search/recipe.js
@@ -21,19 +21,23 @@ module.exports = class RecipeCommand extends Command {
     }
 
     async run(msg, args) {
-        const { query } = args;
-        const { text } = await snekfetch
-            .get('http://www.recipepuppy.com/api/')
-            .query({ q: query });
-        const body = JSON.parse(text);
-        if (!body.results.length) return msg.say('No Results.');
-        const recipe = body.results[Math.floor(Math.random() * body.results.length)];
-        const embed = new MessageEmbed()
-            .setColor(0xC20000)
-            .setURL(recipe.href)
-            .setTitle(recipe.title)
-            .setDescription(`**Ingredients:** ${recipe.ingredients}`)
-            .setThumbnail(recipe.thumbnail);
-        return msg.embed(embed);
+        try {
+            const { query } = args;
+            const { text } = await snekfetch
+                .get('http://www.recipepuppy.com/api/')
+                .query({ q: query });
+            const body = JSON.parse(text);
+            if (!body.results.length) return msg.say('No Results.');
+            const recipe = body.results[Math.floor(Math.random() * body.results.length)];
+            const embed = new MessageEmbed()
+                .setColor(0xC20000)
+                .setURL(recipe.href)
+                .setTitle(recipe.title)
+                .setDescription(`**Ingredients:** ${recipe.ingredients}`)
+                .setThumbnail(recipe.thumbnail);
+            return msg.embed(embed);
+        } catch (err) {
+            return msg.say('No Results.');
+        }
     }
 };
