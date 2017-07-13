8458000bc7ef2f8fa9994eecad3b87706ec9c7d0
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-07-13 00:17:30 +0000

    Update deps

--- structures/Util.js
+++ structures/Util.js
@@ -39,13 +39,6 @@ class Util {
             .then(() => console.log('[DBOTSORG] Successfully posted to Discord Bots Org.'))
             .catch((err) => console.error(`[DBOTSORG] Failed to post to Discord Bots Org. ${err}`));
     }
-
-    static async upvoters(id) {
-        const { body } = await snekfetch
-            .get(`https://discordbots.org/api/bots/${id}/votes`)
-            .set({ Authorization: dbotsOrgKey });
-        return body.map((user) => `${user.username}#${user.discriminator}`);
-    }
 }
 
 module.exports = Util;
