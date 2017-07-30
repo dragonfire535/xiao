9463cd15fa808a5fa4d75f0fe526f618ee186d9d
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-07-30 00:20:59 +0000

    Duh

--- commands/search/pokedex.js
+++ commands/search/pokedex.js
@@ -29,9 +29,6 @@ module.exports = class PokedexCommand extends Command {
 			const { body } = await snekfetch
 				.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`);
 			const id = `${'000'.slice(body.id.toString().length)}${body.id}`;
-			console.log(this.filter(body.names));
-			console.log(this.filter(body.flavor_text_entries));
-			console.log(this.filter(body.genera));
 			const name = this.filter(body.names).name;
 			const flavor = this.filter(body.flavor_text_entries).flavor_text.replace(/\n/g, ' ');
 			const species = this.filter(body.genera).genus;
@@ -52,6 +49,7 @@ module.exports = class PokedexCommand extends Command {
 	}
 
 	filter(arr) {
-		return arr.filter(entry => entry.language.name === 'en')[Math.floor(Math.random() * arr.length)];
+		const filtered = arr.filter(entry => entry.language.name === 'en');
+		return filtered[Math.floor(Math.random() * filtered.length)];
 	}
 };
