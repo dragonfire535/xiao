4d3c509a9806af02383b478cd2344ed596d5a07a
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-08-08 21:21:07 +0000

    Faster Shard Spawns

--- Shard.js
+++ Shard.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
 const path = require('path');
 const { token } = require('./config');
-const Manager = new ShardingManager(path.join(__dirname, 'XiaoBot.js'), { token });
-Manager.spawn();
+const manager = new ShardingManager(path.join(__dirname, 'XiaoBot.js'), { token });
+manager.spawn(undefined, 1000);
