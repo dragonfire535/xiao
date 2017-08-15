6e27e80d34118c4f0c524050e4517bc2beecf141
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-08-15 22:29:32 +0000

    Fix Spaces and Tabs

--- structures/Util.js
+++ structures/Util.js
@@ -63,13 +63,13 @@ class Util {
 	}
 
 	static shuffle(arr) {
-    	for (let i = arr.length - 1; i > 0; i--) {
-        	const j = Math.floor(Math.random() * (i + 1));
-        	const temp = arr[i];
-        	arr[i] = arr[j];
-        	arr[j] = temp;
-    	}
-    	return arr;
+		for (let i = arr.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			const temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+		return arr;
 	}
 }
 
