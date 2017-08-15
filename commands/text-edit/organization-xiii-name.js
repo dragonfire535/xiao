aa38e7cb4d26901abacd4f92bb9090d43d5d954c
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-08-15 22:27:10 +0000

    Shuffle and Nobody Name Commands

---
+++ commands/text-edit/organization-xiii-name.js
@@ -0,0 +1,30 @@
+const Command = require('../../structures/Command');
+const { shuffle } = require('../../structures/Util');
+
+module.exports = class OrganizationXIIINameCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'organization-xiii-name',
+			aliases: ['org-xiii-name', 'xiii-name', 'nobody-name'],
+			group: 'text-edit',
+			memberName: 'organization-xiii-name',
+			description: 'Converts a name into the Organization XIII style.',
+			args: [
+				{
+					key: 'text',
+					prompt: 'What name would you like to convert?',
+					type: 'string',
+					parse: text => text.toLowerCase().split('')
+				}
+			]
+		});
+	}
+
+	run(msg, args) {
+		const { text } = args;
+		text.push('x');
+		const converted = shuffle(text);
+		converted[0] = converted[0].toUpperCase();
+		return msg.say(`\u180E${converted.join('')}`);
+	}
+};
