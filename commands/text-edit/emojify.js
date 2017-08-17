6c7c110a9bc825749befbbc7861da8fc02106de3
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-08-17 01:12:30 +0000

    Emojify Command

---
+++ commands/text-edit/emojify.js
@@ -0,0 +1,31 @@
+const Command = require('../../structures/Command');
+const { letterTrans } = require('custom-translate');
+const dictionary = require('../../assets/json/emojify');
+
+module.exports = class EmojifyCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'emojify',
+			aliases: ['regional-indicator'],
+			group: 'text-edit',
+			memberName: 'emojify',
+			description: 'Converts text to emoji form.',
+			args: [
+				{
+					key: 'text',
+					prompt: 'What text would you like to convert to emoji?',
+					type: 'string',
+					validate: text => {
+						if (letterTrans(text, dictionary).length < 2000) return true;
+						return 'Your text is too long.';
+					}
+				}
+			]
+		});
+	}
+
+	run(msg, args) {
+		const { text } = args;
+		return msg.say(letterTrans(text.toLowerCase(), dictionary));
+	}
+};
