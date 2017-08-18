4e1a9bc13ec38284251eaba183341a3549727a2e
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-08-18 11:33:40 +0000

    Hackban Command (lul)

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -37,6 +37,7 @@ module.exports = class BanCommand extends Command {
 		const modlogs = filterTopics(msg.guild.channels, 'modlog').first();
 		const { member, reason } = args;
 		if (member.id === msg.author.id) return msg.say('I don\'t think you want to ban yourself...');
+		if (member.id === msg.guild.ownerID) return msg.say('Don\'t you think that might be betraying your leader?');
 		if (!member.bannable) return msg.say('This member is not bannable. Perhaps they have a higher role than me?');
 		if (member.highestRole.calculatedPosition > msg.member.highestRole.calculatedPosition - 1) {
 			return msg.say('Your roles are too low to ban this member.');
---
+++ commands/moderation/hackban.js
@@ -0,0 +1,84 @@
+const Command = require('../../structures/Command');
+const { MessageEmbed } = require('discord.js');
+const { stripIndents } = require('common-tags');
+const { filterTopics } = require('../../structures/Util');
+
+module.exports = class HackbanCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'hackban',
+			aliases: ['hackbanne'],
+			group: 'moderation',
+			memberName: 'hackban',
+			description: 'Bans a user who doesn\'t have to be in the server and logs the ban to the mod logs.',
+			guildOnly: true,
+			clientPermissions: ['BAN_MEMBERS'],
+			userPermissions: ['ADMINISTRATOR'],
+			args: [
+				{
+					key: 'id',
+					prompt: 'What is the id of the member you want to hackban?',
+					type: 'string'
+				},
+				{
+					key: 'reason',
+					prompt: 'What do you want to set the reason as?',
+					type: 'string',
+					validate: reason => {
+						if (reason.length < 140) return true;
+						return 'Reason must be under 140 characters.';
+					}
+				}
+			]
+		});
+	}
+
+	async run(msg, args) {
+		const modlogs = filterTopics(msg.guild.channels, 'modlog').first();
+		const { id, reason } = args;
+		if (id === msg.author.id) return msg.say('I don\'t think you want to ban yourself...');
+		if (id === msg.guild.ownerID) return msg.say('Don\'t you think that might be betraying your leader?');
+		let user;
+		try {
+			user = await this.client.fetchUser(id);
+		} catch (err) {
+			return msg.say('Could not resolve user.');
+		}
+		await msg.say(`Are you sure you want to ban ${user.tag} (${user.id})?`);
+		const msgs = await msg.channel.awaitMessages(res => res.author.id === msg.author.id, {
+			max: 1,
+			time: 30000
+		});
+		if (!msgs.size || !['y', 'yes'].includes(msgs.first().content.toLowerCase())) return msg.say('Aborting.');
+		try {
+			await msg.guild.ban(id, {
+				days: 7,
+				reason: `${msg.author.tag}: ${reason}`
+			});
+		} catch (err) {
+			return msg.say(`Could not ban the user: \`${err.message}\``);
+		}
+		await msg.say(`Successfully banned ${user.tag}.`);
+		if (!modlogs) {
+			return msg.say('Could not log the ban to the mod logs.');
+		} else if (modlogs.permissionsFor(this.client.user).has('EMBED_LINKS')) {
+			const embed = new MessageEmbed()
+				.setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+				.setColor(0xA90000)
+				.setTimestamp()
+				.setDescription(stripIndents`
+					**Member:** ${user.tag} (${user.id})
+					**Action:** Hackban
+					**Reason:** ${reason}
+				`);
+			return modlogs.send({ embed });
+		} else {
+			return modlogs.send(stripIndents`
+				**Member:** ${user.tag} (${user.id})
+				**Action:** Hackban
+				**Reason:** ${reason}
+				**Moderator:** ${msg.author.tag}
+			`);
+		}
+	}
+};
