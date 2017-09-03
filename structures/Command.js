2c12f15e1260e6e7e50e82c7a0bc568b8891b411
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-09-03 22:26:43 +0000

    userPermissions and clientPermissions are in commando now

--- structures/Command.js
+++ structures/Command.js
@@ -1,13 +1,10 @@
 const { Command } = require('discord.js-commando');
-const perms = require('../assets/json/permissions');
 
 class XiaoCommand extends Command {
 	constructor(client, info) {
 		super(client, info);
 
 		this.ownerOnly = info.ownerOnly;
-		this.clientPermissions = info.clientPermissions;
-		this.userPermissions = info.userPermissions;
 		this.throttling = info.throttling || {
 			usages: 1,
 			duration: 2
@@ -17,22 +14,6 @@ class XiaoCommand extends Command {
 	hasPermission(msg) {
 		if (this.ownerOnly && !this.client.isOwner(msg.author)) {
 			return `The \`${this.name}\` command can only be used by the bot owner.`;
-		}
-		if (msg.channel.type === 'text') {
-			if (this.clientPermissions) {
-				for (const permission of this.clientPermissions) {
-					if (!msg.channel.permissionsFor(this.client.user).has(permission)) {
-						return `The \`${this.name}\` command requires me to have the \`${perms[permission]}\` permission.`;
-					}
-				}
-			}
-			if (this.userPermissions) {
-				for (const permission of this.userPermissions) {
-					if (!msg.channel.permissionsFor(msg.author).has(permission)) {
-						return `The \`${this.name}\` command requires you to have the \`${perms[permission]}\` permission.`;
-					}
-				}
-			}
 		}
 		return true;
 	}
