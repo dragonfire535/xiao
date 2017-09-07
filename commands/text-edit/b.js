ebfd85eb62bf6eb733e7139cdf8bbc7a3c3426ec
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-09-07 16:51:48 +0000

    🅱

---
+++ commands/text-edit/b.js
@@ -0,0 +1,29 @@
+const Command = require('../../structures/Command');
+
+module.exports = class BCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'b',
+			aliases: [':b:'],
+			group: 'text-edit',
+			memberName: 'b',
+			description: ':b:.',
+			args: [
+				{
+					key: 'text',
+					prompt: 'What text would you like to :b:?',
+					type: 'string',
+					validate: text => {
+						if (text.replace(/(b|d|e|f|g|p|q)/gi, ':b:').length < 2000) return true;
+						return 'Your text is too long';
+					}
+				}
+			]
+		});
+	}
+
+	run(msg, args) {
+		const { text } = args;
+		return msg.say(text.replace(/(b|d|e|f|g|p|q)/gi, ':b:'));
+	}
+};
