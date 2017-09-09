1d5e5298b6f8405abaf4a67deaf901ac2f6504c6
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-09-09 19:18:31 +0000

    Yu Gi Oh Token

---
+++ commands/avatar-edit/yu-gi-oh-token.js
@@ -0,0 +1,48 @@
+const Command = require('../../structures/Command');
+const { createCanvas, loadImage } = require('canvas');
+const snekfetch = require('snekfetch');
+const path = require('path');
+
+module.exports = class YuGiOhTokenCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'yu-gi-oh-token',
+			group: 'avatar-edit',
+			memberName: 'yu-gi-oh-token',
+			description: 'Draws a user\'s avatar over a blank Yu-Gi-Oh! token.',
+			throttling: {
+				usages: 1,
+				duration: 30
+			},
+			clientPermissions: ['ATTACH_FILES'],
+			args: [
+				{
+					key: 'user',
+					prompt: 'Which user would you like to edit the avatar of?',
+					type: 'user',
+					default: ''
+				}
+			]
+		});
+	}
+
+	async run(msg, args) {
+		const user = args.user || msg.author;
+		const avatarURL = user.displayAvatarURL({
+			format: 'png',
+			size: 512
+		});
+		try {
+			const canvas = createCanvas(384, 564);
+			const ctx = canvas.getContext('2d');
+			const base = await loadImage(path.join(__dirname, '..', '..', 'assets', 'images', 'yu-gi-oh-token.png'));
+			const { body } = await snekfetch.get(avatarURL);
+			const avatar = await loadImage(body);
+			ctx.drawImage(base, 0, 0);
+			ctx.drawImage(avatar, 45, 102, 293, 294);
+			return msg.say({ files: [{ attachment: canvas.toBuffer(), name: 'yu-gi-oh-token.png' }] });
+		} catch (err) {
+			return msg.say(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+};
