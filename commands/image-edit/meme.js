1ea2cbf32b5363984c46c7ba952842e15ebe75ba
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-09-16 21:27:22 +0000

    Upgrade Meme Command

--- commands/image-edit/meme.js
+++ commands/image-edit/meme.js
@@ -1,32 +1,27 @@
 const Command = require('../../structures/Command');
 const snekfetch = require('snekfetch');
 const { list } = require('../../structures/Util');
-const codes = require('../../assets/json/meme');
 
 module.exports = class MemeCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'meme',
-			group: 'image-edit',
+			group: 'fun',
 			memberName: 'meme',
 			description: 'Sends a meme with the text and background of your choice.',
 			clientPermissions: ['ATTACH_FILES'],
-			details: `**Codes:** ${codes.join(', ')}`,
 			args: [
 				{
 					key: 'type',
-					prompt: `What meme type do you want to use? Either ${list(codes, 'or')}.`,
+					prompt: 'What meme type do you want to use?',
 					type: 'string',
-					validate: type => {
-						if (codes.includes(type.toLowerCase())) return true;
-						return `Invalid meme type, please enter either ${list(codes, 'or')}.`;
-					},
 					parse: type => type.toLowerCase()
 				},
 				{
 					key: 'top',
 					prompt: 'What should the top row of the meme to be?',
 					type: 'string',
+					default: '_',
 					validate: top => {
 						if (top.length < 200) return true;
 						return 'Invalid top text, please keep the top text under 200 characters.';
@@ -37,6 +32,7 @@ module.exports = class MemeCommand extends Command {
 					key: 'bottom',
 					prompt: 'What should the bottom row of the meme to be?',
 					type: 'string',
+					default: '_',
 					validate: bottom => {
 						if (bottom.length < 200) return true;
 						return 'Invalid bottom text, please keep the bottom text under 200 characters.';
@@ -49,10 +45,17 @@ module.exports = class MemeCommand extends Command {
 
 	async run(msg, { type, top, bottom }) {
 		try {
+			if (type === 'list') {
+				const { body } = await snekfetch
+					.get('https://memegen.link/api/templates/');
+				const codes = Object.values(body).map(code => code.replace('https://memegen.link/api/templates/', ''));
+				return msg.say(list(codes));
+			}
 			const { body } = await snekfetch
 				.get(`https://memegen.link/api/templates/${type}/${top}/${bottom}`, { followRedirects: true });
-			return msg.say(body.direct.masked);
+			return msg.say({ files: [body.direct.masked] });
 		} catch (err) {
+			if (err.status === 404) return msg.say(`Invalid meme type, please use ${msg.usage('list')} for a list.`);
 			return msg.say(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
 		}
 	}
