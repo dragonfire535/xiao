15318de008a8724af11f6b7826e27c0506e35e68
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-09-22 19:26:36 +0000

    Better

--- commands/random-res/cat-fact.js
+++ commands/random-res/cat-fact.js
@@ -16,7 +16,7 @@ module.exports = class CatFactCommand extends Command {
 		try {
 			const { body } = await snekfetch
 				.get('https://fact.birb.pw/api/v1/cat');
-			return msg.say(body.fact);
+			return msg.say(body.string);
 		} catch (err) {
 			return msg.say(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
 		}
