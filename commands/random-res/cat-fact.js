a5e40ec1cad0a656cd030b5e5ea2d7e3aecab96b
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-09-22 19:29:49 +0000

    Revert cat fact

--- commands/random-res/cat-fact.js
+++ commands/random-res/cat-fact.js
@@ -15,8 +15,9 @@ module.exports = class CatFactCommand extends Command {
 	async run(msg) {
 		try {
 			const { body } = await snekfetch
-				.get('https://fact.birb.pw/api/v1/cat');
-			return msg.say(body.string);
+				.get('https://catfact.ninja/fact')
+				.query({ max_length: 2000 });
+			return msg.say(body.fact);
 		} catch (err) {
 			return msg.say(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
 		}
