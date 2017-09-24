53d43508a61a54c98fd8317809ee6e0c96a34ffa
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-09-24 03:46:07 +0000

    Tableflip Command

---
+++ commands/random/tableflip.js
@@ -0,0 +1,30 @@
+const Command = require('../../structures/Command');
+const { wait } = require('../../structures/Util');
+const frames = [
+	'(-°□°)-  ┬─┬',
+	'(╯°□°)╯    ]',
+	'(╯°□°)╯  ︵  ┻━┻',
+	'(╯°□°)╯       [',
+	'(╯°□°)╯           ┬─┬'
+];
+
+module.exports = class TableflipCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'tableflip',
+			aliases: ['a-tableflip', 'animated-tableflip'],
+			group: 'random',
+			memberName: 'tableflip',
+			description: 'Flips a table... With animation!'
+		});
+	}
+
+	async run(msg) {
+		const message = await msg.say('(\\\\°□°)\\\\  ┬─┬');
+		for (const frame of frames) {
+			await wait(400);
+			await message.edit(frame);
+		}
+		return message;
+	}
+};
