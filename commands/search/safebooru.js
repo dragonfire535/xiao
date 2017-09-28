379ae0475dbb7de7882e99d920d35e5d73753a80
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-09-28 18:59:36 +0000

    Beep

--- commands/search/safebooru.js
+++ commands/search/safebooru.js
@@ -35,7 +35,7 @@ module.exports = class SafebooruCommand extends Command {
 			if (parsed._attributes.count === '0' || !parsed.post.length) return msg.say('Could not find any results.');
 			return msg.say(stripIndents`
 				Result for ${query}:
-				http:${parsed.post.posts[Math.floor(Math.random() * parsed.post.posts.length)]._attributes.file_url}
+				http:${parsed.post[Math.floor(Math.random() * parsed.post.length)]._attributes.file_url}
 			`);
 		} catch (err) {
 			return msg.say(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
