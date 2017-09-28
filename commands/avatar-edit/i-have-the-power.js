1a8ab288ad3cc3050a81c6bc4b261ca454125830
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-09-28 01:27:38 +0000

    Beep

--- commands/avatar-edit/i-have-the-power.js
+++ commands/avatar-edit/i-have-the-power.js
@@ -41,7 +41,7 @@ module.exports = class IHaveThePowerCommand extends Command {
 			const avatar = await loadImage(body);
 			ctx.drawImage(base, 0, 0);
 			ctx.rotate(18.3 * (Math.PI / 180));
-			ctx.drawImage(avatar, 325, -6, 175, 175);
+			ctx.drawImage(avatar, 330, -15, 175, 175);
 			ctx.rotate(-18.3 * (Math.PI / 180));
 			return msg.say({ files: [{ attachment: canvas.toBuffer(), name: 'i-have-the-power.png' }] });
 		} catch (err) {
