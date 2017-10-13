20c95d7ec4aecce57d7a6d34e951d0c88a2481ca
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-10-13 22:01:21 +0000

    Beep

--- XiaoBot.js
+++ XiaoBot.js
@@ -11,7 +11,6 @@ const client = new CommandoClient({
 	messageCacheLifetime: 600,
 	messageSweepInterval: 120
 });
-const { postStats } = require('./util/Util');
 const { version } = require('./package');
 const whitelist = require('./assets/json/whitelist');
 
