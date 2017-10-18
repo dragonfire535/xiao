c6ada363646d36f23a62858105b4cfd347fa9238
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-10-18 11:06:34 +0000

    Isn't Joke...

--- commands/single-res/isnt-joke.js
+++ commands/single-res/isnt-joke.js
@@ -3,16 +3,16 @@ const { Command } = require('discord.js-commando');
 module.exports = class ItsJokeCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'its-joke',
-			aliases: ['is-joke'],
+			name: 'isnt-joke',
+			aliases: ['its-not-joke'],
 			group: 'single-res',
-			memberName: 'its-joke',
-			description: 'It\'s joke!',
+			memberName: 'isnt-joke',
+			description: 'Isn\'t joke...',
 			clientPermissions: ['ATTACH_FILES']
 		});
 	}
 
 	run(msg) {
-		return msg.say({ files: ['https://i.imgur.com/NwKdpKK.jpg'] });
+		return msg.say({ files: ['https://i.imgur.com/QdDAeZF.jpg'] });
 	}
 };
