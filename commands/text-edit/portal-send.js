4cfab6e760c6db712f1c95577abb1da8d2b8d37f
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-10-19 00:29:50 +0000

    Portal Send & Nitro

---
+++ commands/text-edit/portal-send.js
@@ -0,0 +1,38 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class PortalSendCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'portal-send',
+			aliases: ['send-portal-message', 'portal-message', 'send-portal-msg', 'portal-msg'],
+			group: 'text-edit',
+			memberName: 'portal-send',
+			description: 'Send a message to a random channel with `<portal>` in the topic.',
+			args: [
+				{
+					key: 'text',
+					prompt: 'What text would you like to send?',
+					type: 'string',
+					validate: text => {
+						if (/discord(\.gg|app\.com\/invite|\.me)\//gi.test(text)) return 'Please do not send invites.';
+						if (text.length < 1000) return true;
+						return 'Invalid text, please keep the text under 1000 characters.';
+					}
+				}
+			]
+		});
+	}
+
+	async run(msg, { text }) {
+		const valid = this.client.channels.filter(channel => channel.type === 'text' && channel.guild.id !== msg.guild.id);
+		const channels = valid.filter(channel => channel.topic && channel.topic.toLowerCase().includes('<portal>'));
+		if (!channels.size) return msg.say('No channels on this shard have an open portal.');
+		const channel = channels.random();
+		try {
+			await channel.send(`**${msg.author.tag} (${msg.guild.name})**: ${text}`);
+			return msg.say(`Message sent to **${channel.name}** in **${channel.guild.name}**!`);
+		} catch (err) {
+			return msg.say('Failed to send the message. Try again later!');
+		}
+	}
+};
