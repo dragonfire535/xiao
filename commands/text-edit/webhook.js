771e1f51f503e4ca8f684cd63d87e7bd93610ba2
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-11-03 23:03:22 +0000

    Add some stuff

---
+++ commands/text-edit/webhook.js
@@ -0,0 +1,34 @@
+const { Command } = require('discord.js-commando');
+const snekfetch = require('snekfetch');
+const { WEBHOOK_URL } = process.env;
+
+module.exports = class WebhookCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'webhook',
+			aliases: ['rin', 'rin-say'],
+			group: 'text-edit',
+			memberName: 'webhook',
+			description: 'Posts a message to the webhook defined in your `process.env`.',
+			ownerOnly: true,
+			clientPermissions: ['MANAGE_MESSAGES'],
+			args: [
+				{
+					key: 'content',
+					prompt: 'What text would you like the webhook to say?',
+					type: 'string'
+				}
+			]
+		});
+	}
+
+	async run(msg, { content }) {
+		if (msg.channel.type === 'text') await msg.delete();
+		try {
+			await snekfetch.post(WEBHOOK_URL).send({ content });
+			return null;
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+};
