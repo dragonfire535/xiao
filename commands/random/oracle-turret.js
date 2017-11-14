d6713d01f1a1fc5d7ca66e8367548b60fcad152e
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-11-14 18:50:35 +0000

    Oracle Turret

---
+++ commands/random/oracle-turret.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const quotes = require('../../assets/json/oracle-turret');
+
+module.exports = class OracleTurretCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'oracle-turret',
+			aliases: ['im-different'],
+			group: 'random',
+			memberName: 'oracle-turret',
+			description: 'Responds with a random Oracle Turret quote.'
+		});
+	}
+
+	run(msg) {
+		return msg.say(quotes[Math.floor(Math.random() * quotes.length)]);
+	}
+};
