843f2925a8fdb0df736987afa6e86e8d3a652090
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-11-19 21:29:41 +0000

    Fix

--- commands/avatar-edit/pixelize.js
+++ commands/avatar-edit/pixelize.js
@@ -6,10 +6,9 @@ module.exports = class PixelizeCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'pixelize',
-			aliases: ['jpeg', 'needs-more-jpeg'],
-			group: 'image-edit',
+			group: 'avatar-edit',
 			memberName: 'pixelize',
-			description: 'Draws an image or a user\'s avatar but pixelized.',
+			description: 'Draws a user\'s avatar but pixelized.',
 			throttling: {
 				usages: 1,
 				duration: 15
@@ -17,29 +16,28 @@ module.exports = class PixelizeCommand extends Command {
 			clientPermissions: ['ATTACH_FILES'],
 			args: [
 				{
-					key: 'image',
-					prompt: 'What image would you like to edit?',
-					type: 'image',
+					key: 'user',
+					prompt: 'Which user would you like to edit the avatar of?',
+					type: 'user',
 					default: ''
 				}
 			]
 		});
 	}
 
-	async run(msg, { image }) {
-		if (!image) {
-			image = msg.author.displayAvatarURL({
-				format: 'png',
-				size: 64
-			});
-		}
+	async run(msg, { user }) {
+		if (!user) user = msg.author;
+		const avatarURL = user.displayAvatarURL({
+			format: 'png',
+			size: 64
+		});
 		try {
-			const { body } = await snekfetch.get(image);
-			const data = await loadImage(body);
+			const { body } = await snekfetch.get(avatarURL);
+			const avatar = await loadImage(body);
 			const canvas = createCanvas(512, 512);
 			const ctx = canvas.getContext('2d');
 			ctx.imageSmoothingEnabled = false;
-			ctx.drawImage(data, 0, 0, 512, 512);
+			ctx.drawImage(avatar, 0, 0, 512, 512);
 			return msg.say({ files: [{ attachment: canvas.toBuffer(), name: 'pixelize.png' }] });
 		} catch (err) {
 			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
