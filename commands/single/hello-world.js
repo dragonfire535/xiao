a8903613ac2608a6bb15e9d6601dfef55ab97c06
zakt0210 <zakt0210@hotmail.com>
2017-12-06 12:27:51 -0500

    Hello World Command

---
+++ commands/single/hello-world.js
@@ -0,0 +1,17 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class HelloWorldCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'hello-word',
+      aliases: ['hola-mundo'],
+			group: 'single',
+			memberName: 'hello-world',
+			description: 'Hello world.'
+		});
+	}
+
+	run(msg) {
+		return msg.say('Hello World!');
+	}
+};
