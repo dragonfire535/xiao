9a7846b9199233f1a5520e76612f1367a341d107
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-12-13 18:22:47 +0000

    Fix

--- commands/image-edit/ifunny.js
+++ commands/image-edit/ifunny.js
@@ -1,6 +1,7 @@
 const { Command } = require('discord.js-commando');
 const { createCanvas, loadImage } = require('canvas');
 const snekfetch = require('snekfetch');
+const path = require('path');
 
 module.exports = class IfunnyCommand extends Command {
 	constructor(client) {
@@ -27,13 +28,15 @@ module.exports = class IfunnyCommand extends Command {
 
 	async run(msg, { image }) {
 		try {
+			const base = loadImage(path.join(__dirname, '..', '..', 'assets', 'images', 'ifunny.png'));
 			const { body } = await snekfetch.get(image);
 			const data = await loadImage(body);
 			const canvas = createCanvas(data.width, data.height);
 			const ctx = canvas.getContext('2d');
+			ctx.drawImage(data, 0, 0);
 			ctx.fillStyle = '#181619';
 			ctx.fillRect(0, canvas.height - data.height, canvas.width, data.height);
-			ctx.drawImage(data, canvas.width - data.width, canvas.height - data.height);
+			ctx.drawImage(base, canvas.width - data.width, canvas.height - data.height);
 			return msg.say({ files: [{ attachment: canvas.toBuffer(), name: 'ifunny.png' }] });
 		} catch (err) {
 			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
