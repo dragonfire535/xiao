c251b89310f48592ea2cc5ccf98ae4ae9c4c6184
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-12-13 18:21:20 +0000

    iFunny command

---
+++ commands/image-edit/ifunny.js
@@ -0,0 +1,42 @@
+const { Command } = require('discord.js-commando');
+const { createCanvas, loadImage } = require('canvas');
+const snekfetch = require('snekfetch');
+
+module.exports = class IfunnyCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'ifunny',
+			group: 'image-edit',
+			memberName: 'ifunny',
+			description: 'Draws an image with the iFunny logo.',
+			throttling: {
+				usages: 1,
+				duration: 10
+			},
+			clientPermissions: ['ATTACH_FILES'],
+			args: [
+				{
+					key: 'image',
+					prompt: 'What image would you like to edit?',
+					type: 'image',
+					default: msg => msg.author.displayAvatarURL({ format: 'png', size: 512 })
+				}
+			]
+		});
+	}
+
+	async run(msg, { image }) {
+		try {
+			const { body } = await snekfetch.get(image);
+			const data = await loadImage(body);
+			const canvas = createCanvas(data.width, data.height);
+			const ctx = canvas.getContext('2d');
+			ctx.fillStyle = '#181619';
+			ctx.fillRect(0, canvas.height - data.height, canvas.width, data.height);
+			ctx.drawImage(data, canvas.width - data.width, canvas.height - data.height);
+			return msg.say({ files: [{ attachment: canvas.toBuffer(), name: 'ifunny.png' }] });
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+};
