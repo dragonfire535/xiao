e815395baea1c1cf844cebc22632c50732e2e226
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-12-13 18:56:47 +0000

    QR Code Command

---
+++ commands/text-edit/qr-code.js
@@ -0,0 +1,34 @@
+const { Command } = require('discord.js-commando');
+const snekfetch = require('snekfetch');
+
+module.exports = class QRCodeCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'qr-code',
+			group: 'text-edit',
+			memberName: 'qr-code',
+			description: 'Converts text to a QR Code.',
+			args: [
+				{
+					key: 'text',
+					prompt: 'What text would you like to convert to a QR Code?',
+					type: 'string'
+				}
+			]
+		});
+	}
+
+	async run(msg, { text }) {
+		try {
+			const { body } = await snekfetch
+				.get('https://api.qrserver.com/v1/create-qr-code/')
+				.query({
+					data: text,
+					size: '500x500'
+				});
+			return msg.say({ files: [{ attachment: body, name: 'qr-code.png' }] });
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+};
