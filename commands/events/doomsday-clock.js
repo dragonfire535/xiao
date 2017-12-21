d936b07a4c222860d78dded9131a1549f20a7812
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-12-21 17:38:18 +0000

    Doomsday Clock Command

---
+++ commands/events/doomsday-clock.js
@@ -0,0 +1,28 @@
+const { Command } = require('discord.js-commando');
+const snekfetch = require('snekfetch');
+const { stripIndents } = require('common-tags');
+
+module.exports = class DoomsdayClockCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'doomsday-clock',
+			group: 'events',
+			memberName: 'doomsday-clock',
+			description: 'Responds with the current time of the Doomsday Clock.'
+		});
+	}
+
+	async run(msg) {
+		try {
+			const { text } = await snekfetch.get('https://thebulletin.org/timeline');
+			const time = text.match(/IT IS(?: STILL)? (.+) MINUTES TO MIDNIGHT/)[0];
+			const desc = text.match(/<div class="body-text"><span class="timeline-year">(.+)<\/span>: (.+)<\/div>/);
+			return msg.say(stripIndents`
+				**${time}**
+				${desc[1]}: ${desc[2].replace(/<a href="(?:.+)">(.+)<\/a>/g, '$1').replace(/<\/div>/g, '')}
+			`);
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+};
