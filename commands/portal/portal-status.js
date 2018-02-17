14c9c5c0a03b9e64ce69c848e08eef04c837a083
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-02-17 22:17:13 +0000

    Fix

--- commands/portal/portal-status.js
+++ commands/portal/portal-status.js
@@ -12,7 +12,7 @@ module.exports = class PortalStatusCommand extends Command {
 
 	run(msg) {
 		const channels = this.client.provider.get('global', 'portals', []);
-		const local = msg.channel.type === 'text' ? channels.filter(c => !msg.guild.channels.has(c.id)).length : null;
+		const local = msg.channel.type === 'text' ? channels.filter(c => msg.guild.channels.has(c.id)).length : 0;
 		return msg.say(
 			`There are currently **${channels.length}** open portals${msg.channel.type === 'text'
 				? `, **${local}** of which are in this server.`
