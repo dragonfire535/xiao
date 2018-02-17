2ea7c09361a9d9b13b37b3106cafd57bc5b1c08c
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-02-17 22:12:16 +0000

    portal status

---
+++ commands/portal/portal-status.js
@@ -0,0 +1,22 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class PortalStatusCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'portal-status',
+			group: 'portal',
+			memberName: 'portal-status',
+			description: 'Shows the number of currently opened portals.'
+		});
+	}
+
+	run(msg) {
+		const channels = this.client.provider.get('global', 'portals', []);
+		const local = msg.channel.type === 'text' ? channels.filter(c => !msg.guild.channels.has(c.id)).size : null;
+		return msg.say(
+			`There are currently **${channels.length}** open portals${msg.channel.type === 'text'
+				? `, **${local}** of which are in this server.`
+				: '.'}`
+		);
+	}
+};
