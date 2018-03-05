3c6ae829e549a889e034c75501805667561b3268
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-03-05 22:49:58 +0000

    Fix

--- commands/search/danbooru.js
+++ commands/search/danbooru.js
@@ -34,7 +34,7 @@ module.exports = class DanbooruCommand extends Command {
 					limit: 1
 				});
 			if (!body.length || !body[0].file_url) return msg.say('Could not find any results.');
-			return msg.say(`https://danbooru.donmai.us${body[0].file_url}`);
+			return msg.say(body[0].file_url);
 		} catch (err) {
 			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
 		}
