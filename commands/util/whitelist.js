58449d85dd6d5e69ab7640983253ef22da0b1bdc
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-03-20 22:14:18 +0000

    Fix

--- commands/util/whitelist.js
+++ commands/util/whitelist.js
@@ -20,7 +20,7 @@ module.exports = class WhitelistCommand extends Command {
 	}
 
 	run(msg, { user }) {
-		if (this.client.isOwner(msg.author)) return msg.reply('The bot owner cannot be blacklisted.');
+		if (this.client.isOwner(user)) return msg.reply('The bot owner cannot be blacklisted.');
 		if (user.bot) return msg.reply('Bots cannot be blacklisted.');
 		const blacklist = this.client.provider.get('global', 'blacklist', []);
 		if (!blacklist.includes(user.id)) return msg.reply(`${user.tag} is not blacklisted!`);
