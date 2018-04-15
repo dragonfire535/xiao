5e144bd24cfdfafd6a87fe9f51f1b1b27ed78a40
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-04-15 01:21:26 +0000

    Improve coolness

--- commands/analyze/coolness.js
+++ commands/analyze/coolness.js
@@ -1,5 +1,5 @@
 const { Command } = require('discord.js-commando');
-const levels = require('../../assets/json/coolness');
+const texts = require('../../assets/json/coolness');
 
 module.exports = class CoolnessCommand extends Command {
 	constructor(client) {
@@ -20,14 +20,14 @@ module.exports = class CoolnessCommand extends Command {
 	}
 
 	run(msg, { user }) {
+		const authorUser = user.id === msg.author.id;
 		const coolness = Math.round(((user.id / this.client.user.id) * 10) / 2);
-		if (user.id === this.client.user.id) return msg.say('Me? I think I\'m the very best, like no one ever was.');
+		if (user.id === this.client.user.id) return msg.reply('Me? I think I\'m the very best, like no one ever was.');
 		if (this.client.isOwner(user)) {
-			if (this.client.isOwner(msg.author)) {
-				return msg.say(`${user.username}, you're the best owner a bot could ask for! ❤`);
-			}
-			return msg.say(`Don't tell them I said this but I think ${user.username} ${levels[levels.length - 1]}`);
+			if (authorUser) return msg.reply('You\'re the best owner a bot could ask for! ❤');
+			return msg.reply(`Don't tell them I said this but I think ${user.username} smells like a sack of diapers.`);
 		}
-		return msg.say(`${user.username} ${levels[Math.min(coolness, levels.length - 1)]}`);
+		const text = texts[Math.min(coolness, texts.length - 1)];
+		return msg.reply(`${authorUser ? 'You are' : `${user.username} is`} ${text}`);
 	}
 };
