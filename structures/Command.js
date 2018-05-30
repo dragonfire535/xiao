2aa5b0f052304ec502fccd90907a19e4e7d3767d
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-05-30 12:25:07 +0000

    Last try

--- structures/Command.js
+++ structures/Command.js
@@ -2,9 +2,9 @@ const { Command } = require('discord.js-commando');
 
 class XiaoCommand extends Command {
 	constructor(client, info) {
+		if (typeof info.argsPromptLimit === 'undefined') info.argsPromptLimit = 1;
 		super(client, info);
 
-		this.argsPromptLimit = info.argsPromptLimit || 1;
 		this.argsSingleQuotes = info.argsSingleQuotes || false;
 		this.throttling = info.throttling || { usages: 1, duration: 2 };
 	}
