43a678dc3a2196d50ff44e719af9264f4a92a21f
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-07-07 19:53:30 +0000

    Allow usage of a personal event calendar alongside the holidays one

--- commands/events/holidays.js
+++ commands/events/holidays.js
@@ -1,13 +1,14 @@
 const Command = require('../../structures/Command');
 const request = require('node-superfetch');
-const { today, tomorrow } = require('../../util/Util');
-const { GOOGLE_KEY, GOOGLE_CALENDAR_ID } = process.env;
+const { stripIndents } = require('common-tags');
+const { list, today, tomorrow } = require('../../util/Util');
+const { GOOGLE_KEY, GOOGLE_CALENDAR_ID, PERSONAL_GOOGLE_CALENDAR_ID } = process.env;
 
 module.exports = class HolidaysCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'holidays',
-			aliases: ['google-calendar', 'holiday'],
+			aliases: ['google-calendar', 'holiday', 'calendar'],
 			group: 'events',
 			memberName: 'holidays',
 			description: 'Responds with today\'s holidays.'
@@ -16,20 +17,38 @@ module.exports = class HolidaysCommand extends Command {
 
 	async run(msg) {
 		try {
+			const events = [];
+			const standardEvents = await this.fetchHolidays(GOOGLE_CALENDAR_ID);
+			if (standardEvents) events.push(...standardEvents);
+			const personalEvents = await this.fetchHolidays(PERSONAL_GOOGLE_CALENDAR_ID);
+			if (personalEvents) events.push(...personalEvents);
+			if (!events.length) return msg.say('There are no holidays today...');
+			return msg.say(stripIndents`
+				There are **${events.length}** holidays today:
+				${list(events)}.
+			`);
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+
+	async fetchHolidays(id) {
+		try {
 			const { body } = await request
-				.get(`https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(GOOGLE_CALENDAR_ID)}/events`)
+				.get(`https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(id)}/events`)
 				.query({
-					maxResults: 10,
+					maxResults: 20,
 					orderBy: 'startTime',
 					singleEvents: true,
 					timeMax: tomorrow().toISOString(),
 					timeMin: today().toISOString(),
 					key: GOOGLE_KEY
 				});
-			if (!body.items.length) return msg.say('There are no holidays today...');
-			return msg.say(body.items.map(holiday => holiday.summary).join('\n'));
+			if (!body.items.length) return null;
+			return body.items.map(holiday => holiday.summary);
 		} catch (err) {
-			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+			if (err.status === 404) return null;
+			throw err;
 		}
 	}
 };
