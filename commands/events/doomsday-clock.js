db96889feab98f891f9c8b4b434a57e68ae54cea
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-07-07 03:45:50 +0000

    should be current time

--- commands/events/doomsday-clock.js
+++ commands/events/doomsday-clock.js
@@ -23,7 +23,7 @@ module.exports = class DoomsdayClockCommand extends Command {
 				.setTitle(`${year}: ${time}`)
 				.setColor(0x000000)
 				.setThumbnail(image)
-				.setURL('https://thebulletin.org/doomsday-clock/past-announcements/')
+				.setURL('https://thebulletin.org/doomsday-clock/current-time/')
 				.setAuthor('Bulletin of the Atomic Scientists', undefined, 'https://thebulletin.org/')
 				.setDescription(description.replace(/<a href="(.+)">(.+)<\/a>/, '[$2]($1)'));
 			return msg.embed(embed);
