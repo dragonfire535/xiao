42ca43e1bad5b732fcdefc5b49fdd9e73c53c0bc
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-08-29 23:53:42 +0000

    Make webhook a part of client

--- commands/text-edit/webhook.js
+++ commands/text-edit/webhook.js
@@ -1,6 +1,4 @@
 const Command = require('../../structures/Command');
-const request = require('node-superfetch');
-const { WEBHOOK_ID, WEBHOOK_TOKEN } = process.env;
 
 module.exports = class WebhookCommand extends Command {
 	constructor(client) {
@@ -9,7 +7,7 @@ module.exports = class WebhookCommand extends Command {
 			aliases: ['rin', 'rin-say'],
 			group: 'text-edit',
 			memberName: 'webhook',
-			description: 'Posts a message to the webhook defined in your `process.env`.',
+			description: 'Posts a message to the webhook defined in the bot owner\'s `process.env`.',
 			details: 'Only the bot owner(s) may use this command.',
 			ownerOnly: true,
 			clientPermissions: ['MANAGE_MESSAGES'],
@@ -26,9 +24,7 @@ module.exports = class WebhookCommand extends Command {
 	async run(msg, { content }) {
 		try {
 			if (msg.channel.type === 'text' && msg.deletable) await msg.delete();
-			await request
-				.post(`https://discordapp.com/api/webhooks/${WEBHOOK_ID}/${WEBHOOK_TOKEN}`)
-				.send({ content });
+			await this.client.webhook.send(content);
 			return null;
 		} catch (err) {
 			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
--- structures/Client.js
+++ structures/Client.js
@@ -1,10 +1,13 @@
 const { CommandoClient } = require('discord.js-commando');
+const { WebhookClient } = require('discord.js');
 const PokemonStore = require('./PokemonStore');
+const { XIAO_WEBHOOK_ID, XIAO_WEBHOOK_TOKEN } = process.env;
 
 module.exports = class XiaoClient extends CommandoClient {
 	constructor(options) {
 		super(options);
 
+		this.webhook = new WebhookClient(XIAO_WEBHOOK_ID, XIAO_WEBHOOK_TOKEN, { disableEveryone: true });
 		this.pokemon = new PokemonStore();
 	}
 };
