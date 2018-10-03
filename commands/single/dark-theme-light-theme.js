4567a8c14a9f3da467d5ff74e76045e5e0fc5cdd
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-10-03 22:54:17 +0000

    Aliases

--- commands/single/dark-theme-light-theme.js
+++ commands/single/dark-theme-light-theme.js
@@ -7,7 +7,7 @@ module.exports = class DarkThemeLightThemeCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'dark-theme-light-theme',
-			aliases: ['light-theme-dark-theme', 'dark-theme', 'light-theme'],
+			aliases: ['light-theme-dark-theme', 'dark-theme', 'light-theme', 'dark-light', 'dtlt'],
 			group: 'single',
 			memberName: 'dark-theme-light-theme',
 			description: 'Determines whether you use dark or light theme.',
