8c6d3fee08b84eaed00ad93139081a1d1934ec34
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-10-04 16:48:30 +0000

    More convincing fly

--- commands/single/fly.js
+++ commands/single/fly.js
@@ -1,5 +1,7 @@
 const Command = require('../../structures/Command');
 const path = require('path');
+const { list } = require('../../util/Util');
+const types = ['default', 'blank'];
 
 module.exports = class FlyCommand extends Command {
 	constructor(client) {
@@ -9,11 +11,22 @@ module.exports = class FlyCommand extends Command {
 			group: 'single',
 			memberName: 'fly',
 			description: 'Sends a fake fly that looks surprisngly real.',
-			clientPermissions: ['ATTACH_FILES']
+			details: `**Types:** ${types.join(', ')}`,
+			clientPermissions: ['ATTACH_FILES'],
+			args: [
+				{
+					key: 'type',
+					prompt: `What type of meme do you want to use? Either ${list(types, 'or')}.`,
+					type: 'string',
+					default: 'default',
+					oneOf: types,
+					parse: type => type.toLowerCase()
+				}
+			]
 		});
 	}
 
-	run(msg) {
-		return msg.say({ files: [path.join(__dirname, '..', '..', 'assets', 'images', 'fly.png')] });
+	run(msg, { type }) {
+		return msg.say({ files: [path.join(__dirname, '..', '..', 'assets', 'images', 'fly', `${type}.png`)] });
 	}
 };
