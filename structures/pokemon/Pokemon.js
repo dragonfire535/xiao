4120f195c616739327a5ca432ca658080a0162b9
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-10-18 00:41:32 +0000

    Clean-ups

--- structures/pokemon/Pokemon.js
+++ structures/pokemon/Pokemon.js
@@ -1,11 +1,11 @@
 module.exports = class Pokemon {
 	constructor(data) {
 		this.id = data.id;
-		this.name = data.names.filter(entry => entry.language.name === 'en')[0].name;
+		this.name = data.names.find(entry => entry.language.name === 'en').name;
 		this.entries = data.flavor_text_entries
 			.filter(entry => entry.language.name === 'en')
 			.map(entry => entry.flavor_text.replace(/\n|\f|\r/g, ' '));
-		this.names = data.names;
+		this.names = data.names.map(entry => ({ name: entry.name, language: entry.language.name }));
 		this.genus = `The ${data.genera.filter(entry => entry.language.name === 'en')[0].genus}`;
 	}
 
