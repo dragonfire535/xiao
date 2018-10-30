060b5db03bf7e8404b47b51a1b122627b06fd324
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-10-30 22:34:27 +0000

    Make Anime and Manga use AniList

---
+++ commands/search/anime.js
@@ -0,0 +1,101 @@
+const Command = require('../../structures/Command');
+const { MessageEmbed } = require('discord.js');
+const request = require('node-superfetch');
+const { stripIndents } = require('common-tags');
+const { shorten } = require('../../util/Util');
+const searchGraphQL = stripIndents`
+	query ($search: String, $type: MediaType, $isAdult: Boolean) {
+		anime: Page (perPage: 1) {
+			results: media (type: $type, isAdult: $isAdult, search: $search) { id }
+		}
+	}
+`;
+const resultGraphQL = stripIndents`
+	query media($id: Int, $type: MediaType) {
+		Media(id: $id, type: $type) {
+			id
+			title { userPreferred }
+			coverImage { large }
+			startDate { year }
+			description
+			season
+			type
+			status
+			episodes
+			isAdult
+			averageScore
+		}
+	}
+`;
+
+module.exports = class AnimeCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'anime',
+			aliases: ['anilist-anime', 'anilist', 'mal'],
+			group: 'search',
+			memberName: 'anime',
+			description: 'Searches AniList for your query, getting anime results.',
+			clientPermissions: ['EMBED_LINKS'],
+			args: [
+				{
+					key: 'query',
+					prompt: 'What anime would you like to search for?',
+					type: 'string'
+				}
+			]
+		});
+	}
+
+	async run(msg, { query }) {
+		try {
+			const id = await this.search(query, msg.channel.nsfw);
+			if (!id) return msg.say('Could not find any results.');
+			const anime = await this.fetchAnime(id);
+			const embed = new MessageEmbed()
+				.setColor(0x02A9FF)
+				.setAuthor('AniList', 'https://i.imgur.com/iUIRC7v.png', 'https://anilist.co/')
+				.setURL(`https://anilist.co/anime/${anime.id}`)
+				.setThumbnail(anime.coverImage.large || null)
+				.setTitle(anime.title.userPreferred)
+				.setDescription(shorten(anime.description))
+				.addField('❯ Type', `${anime.type} - ${anime.status}`, true)
+				.addField('❯ Episodes', anime.episodes, true)
+				.addField('❯ Season', `${anime.season} ${anime.startDate.year}`, true)
+				.addField('❯ Average Score', `${anime.averageScore}/10`, true)
+				.addField('❯ Adult?', anime.isAdult ? 'Yes' : 'No', true)
+				.addField('❯ Average Score', `${anime.averageScore}/10`, true);
+			return msg.embed(embed);
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+
+	async search(query, nsfw) {
+		const { body } = await request
+			.post('https://graphql.anilist.co/')
+			.send({
+				variables: {
+					search: query,
+					type: 'ANIME',
+					isAdult: Boolean(nsfw)
+				},
+				query: searchGraphQL
+			});
+		if (!body.data.anime.results.length) return null;
+		return body.data.anime.results[0].id;
+	}
+
+	async fetchAnime(id) {
+		const { body } = await request
+			.post('https://graphql.anilist.co/')
+			.send({
+				variables: {
+					id,
+					type: 'ANIME'
+				},
+				query: resultGraphQL
+			});
+		return body.data.Media;
+	}
+};
---
+++ commands/search/manga.js
@@ -0,0 +1,101 @@
+const Command = require('../../structures/Command');
+const { MessageEmbed } = require('discord.js');
+const request = require('node-superfetch');
+const { stripIndents } = require('common-tags');
+const { shorten } = require('../../util/Util');
+const searchGraphQL = stripIndents`
+	query ($search: String, $type: MediaType, $isAdult: Boolean) {
+		anime: Page (perPage: 1) {
+			results: media (type: $type, isAdult: $isAdult, search: $search) { id }
+		}
+	}
+`;
+const resultGraphQL = stripIndents`
+	query media($id: Int, $type: MediaType) {
+		Media(id: $id, type: $type) {
+			id
+			title { userPreferred }
+			coverImage { large }
+			startDate { year }
+			description
+			type
+			status
+			volumes
+			chapters
+			isAdult
+			averageScore
+		}
+	}
+`;
+
+module.exports = class MangaCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'manga',
+			aliases: ['anilist-manga'],
+			group: 'search',
+			memberName: 'manga',
+			description: 'Searches AniList for your query, getting manga results.',
+			clientPermissions: ['EMBED_LINKS'],
+			args: [
+				{
+					key: 'query',
+					prompt: 'What manga would you like to search for?',
+					type: 'string'
+				}
+			]
+		});
+	}
+
+	async run(msg, { query }) {
+		try {
+			const id = await this.search(query, msg.channel.nsfw);
+			if (!id) return msg.say('Could not find any results.');
+			const manga = await this.fetchAnime(id);
+			const embed = new MessageEmbed()
+				.setColor(0x02A9FF)
+				.setAuthor('AniList', 'https://i.imgur.com/iUIRC7v.png', 'https://anilist.co/')
+				.setURL(`https://anilist.co/manga/${manga.id}`)
+				.setThumbnail(manga.coverImage.large || null)
+				.setTitle(manga.title.userPreferred)
+				.setDescription(shorten(manga.description))
+				.addField('❯ Type', `${manga.type} - ${manga.status}`, true)
+				.addField('❯ Chapters / Volumes', `${manga.chapters}/${manga.volumes}`, true)
+				.addField('❯ Year', manga.startDate.year, true)
+				.addField('❯ Average Score', `${manga.averageScore}/10`, true)
+				.addField('❯ Adult?', manga.isAdult ? 'Yes' : 'No', true)
+				.addField('❯ Average Score', `${manga.averageScore}/10`, true);
+			return msg.embed(embed);
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+
+	async search(query, nsfw) {
+		const { body } = await request
+			.post('https://graphql.anilist.co/')
+			.send({
+				variables: {
+					search: query,
+					type: 'MANGA',
+					isAdult: Boolean(nsfw)
+				},
+				query: searchGraphQL
+			});
+		if (!body.data.anime.results.length) return null;
+		return body.data.anime.results[0].id;
+	}
+
+	async fetchAnime(id) {
+		const { body } = await request
+			.post('https://graphql.anilist.co/')
+			.send({
+				variables: {
+					id,
+					type: 'MANGA'
+				},
+				query: resultGraphQL
+			});
+		return body.data.Media;
+	}
+};
