618344db000c3d48cdf74b69e426edb8b8a97b49
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-11-03 18:36:19 +0000

    Update ping to latest master

--- commands/util/ping.js
+++ commands/util/ping.js
@@ -19,7 +19,7 @@ module.exports = class PingCommand extends Command {
 		const ping = Math.round(message.createdTimestamp - msg.createdTimestamp);
 		return message.edit(stripIndents`
 			🏓 P${'o'.repeat(Math.min(Math.round(ping / 100), 1500))}ng! \`${formatNumber(ping)}ms\`
-			Heartbeat: \`${formatNumber(Math.round(this.client.ping))}ms\`
+			Heartbeat: \`${formatNumber(Math.round(this.client.ws.ping))}ms\`
 		`);
 	}
 };
