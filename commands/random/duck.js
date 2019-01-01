f0cc3f5ad821e8adec97e140e1a87af48f7ffda0
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2019-01-01 19:44:43 +0000

    Fix Duck

--- commands/random/duck.js
+++ commands/random/duck.js
@@ -15,7 +15,7 @@ module.exports = class DuckCommand extends Command {
 
 	async run(msg) {
 		try {
-			const { body } = await request.get('https://api.random-d.uk/random');
+			const { body } = await request.get('https://random-d.uk/api/v1/random');
 			return msg.say({ files: [body.url] });
 		} catch (err) {
 			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
