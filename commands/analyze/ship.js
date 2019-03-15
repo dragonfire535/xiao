d21fae5a9a9fb69beb98bdc19acba5b0816f830a
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2019-03-15 14:34:32 +0000

    Improve ship command

--- commands/analyze/ship.js
+++ commands/analyze/ship.js
@@ -29,14 +29,17 @@ module.exports = class ShipCommand extends Command {
 
 	run(msg, { first, second }) {
 		if (first.id === second.id) return msg.reply('Shipping someone with themselves would be pretty weird.');
-		const botText = first.id === this.client.user.id || second.id === this.client.user.id
-			? `But ${first.id === msg.author.id || second.id === msg.author.id ? 'you\'re' : 'they\'re'} still rejected.`
-			: '';
+		const authorInvolved = first.id === msg.author.id || second.id === msg.author.id;
 		const random = new Random(Random.engines.mt19937().seed(Math.abs(first.id - second.id)));
 		const level = random.integer(0, 100);
+		const botText = first.id === this.client.user.id || second.id === this.client.user.id
+			? level >= 70
+				? `But ${authorInvolved ? 'you\'re' : 'they\'re'} still rejected.`
+				: `Haha even a bot thinks ${authorInvolved ? 'you\'re' : 'they\'re'} lame.`
+			: '';
 		return msg.say(oneLine`
 			${first.id === this.client.user.id ? 'Me' : first.username} and
-			${second.id === this.client.user.id ? 'me' : second.username} have a compatability of... **${level}%**! ${botText}
+			${second.id === this.client.user.id ? 'I' : second.username} have a compatability of... **${level}%**! ${botText}
 		`);
 	}
 };
