e25dce4123acd7fc0f8ec1d6c94da1db354a59f7
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2019-04-04 14:06:31 +0000

    no u

---
+++ commands/single/no-u.js
@@ -0,0 +1,18 @@
+const Command = require('../../structures/Command');
+
+module.exports = class NoUCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'no-u',
+			aliases: ['no-you'],
+			group: 'single',
+			memberName: 'no-u',
+			description: 'no u',
+			patterns: [/no (you|u)/i]
+		});
+	}
+
+	run(msg) {
+		return msg.reply('no u');
+	}
+};
