9ab7ac1e2008c41e4b82bb9f8c0e87a590f022ea
Jeremiah <43737601+bullfrog0981@users.noreply.github.com>
2019-12-08 21:10:06 -0500

    Allow urls ending to be used as images
    This will allow commands that use the image argument type to accept urls that end in PNG, JPG, or GIF

--- types/image.js
+++ types/image.js
@@ -1,18 +1,22 @@
 const { ArgumentType } = require('discord.js-commando');
 const fileTypeRe = /\.(jpe?g|png|gif)$/i;
+const request = require('node-superfetch')
 
 module.exports = class ImageArgumentType extends ArgumentType {
 	constructor(client) {
 		super(client, 'image');
 	}
 
-	validate(value, msg, arg) {
+	async validate(value, msg, arg) {
 		const attachment = msg.attachments.first();
 		if (attachment) {
 			if (!attachment.height || !attachment.width) return false;
 			if (attachment.size > 8e+6) return 'Please provide an image under 8 MB.';
 			if (!fileTypeRe.test(attachment.name)) return 'Please only send PNG, JPG, or GIF format images.';
 			return true;
+  } else if  (fileTypeRe.test(value.toLowerCase())) {
+		 try{ await request.get(value) }catch(err){ return false; }
+		      return true;
 		}
 		return this.client.registry.types.get('user').validate(value, msg, arg);
 	}
@@ -20,6 +24,7 @@ module.exports = class ImageArgumentType extends ArgumentType {
 	async parse(value, msg, arg) {
 		const attachment = msg.attachments.first();
 		if (attachment) return attachment.url;
+		if (fileTypeRe.test(value.toLowerCase())) return value;
 		const user = await this.client.registry.types.get('user').parse(value, msg, arg);
 		return user.displayAvatarURL({ format: 'png', size: 512 });
 	}
