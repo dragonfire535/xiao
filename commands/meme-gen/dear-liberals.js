1f714a7574a28e3ed0bb1667b20e6d79161c8e4d
Dragon Fire <danielbodendahl@gmail.com>
2019-12-18 16:16:55 -0500

    Fix

--- commands/meme-gen/dear-liberals.js
+++ commands/meme-gen/dear-liberals.js
@@ -64,7 +64,7 @@ module.exports = class DearLiberalsCommand extends Command {
 		ctx.textBaseline = 'top';
 		ctx.font = '20px Oswald-SemiBold';
 		ctx.rotate(12.30 * (Math.PI / 180));
-		ctx.fillText(`#${hashtag}`, 86, 325);
+		ctx.fillText(`#${hashtag}`, 186, 225);
 		ctx.rotate(-12.30 * (Math.PI / 180));
 		ctx.fillStyle = '#002046';
 		ctx.font = '27px Oswald-SemiBold';
