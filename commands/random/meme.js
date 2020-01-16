b18dbee1e92c9008f369c2f5d93a4f84b92c8d1f
Dragon Fire <danielbodendahl@gmail.com>
2020-01-16 00:31:49 -0500

    Fix

--- commands/random/meme.js
+++ commands/random/meme.js
@@ -27,10 +27,10 @@ module.exports = class MemeCommand extends SubredditCommand {
 		});
 	}
 
-	generateText(post, subreddit) {
+	generateText(post, subreddit, icon) {
 		return new MessageEmbed()
 			.setColor(0xFF4500)
-			.setAuthor(`r/${subreddit}`, post.icon, `https://www.reddit.com/r/${subreddit}/`)
+			.setAuthor(`r/${subreddit}`, icon, `https://www.reddit.com/r/${subreddit}/`)
 			.setTitle(post.title)
 			.setImage(post.post_hint === 'image' ? post.url : null)
 			.setURL(`https://www.reddit.com${post.permalink}`)
