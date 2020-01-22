11412be037fc43f44d065f9fdbb427917384b0f4
Dragon Fire <danielbodendahl@gmail.com>
2020-01-22 11:36:26 -0500

    Fix HTML Tags in Doomsday Clock

--- commands/events/doomsday-clock.js
+++ commands/events/doomsday-clock.js
@@ -25,13 +25,15 @@ module.exports = class DoomsdayClockCommand extends Command {
 			const { text } = await request.get('https://thebulletin.org/doomsday-clock/past-announcements/');
 			const time = text.match(/<h3 class="uabb-infobox-title">(.+)<\/h3>/)[1];
 			const year = text.match(/<h5 class="uabb-infobox-title-prefix">(.+)<\/h5>/)[1];
-			const description = text.match(/<div class="uabb-infobox-text uabb-text-editor"><p>(.+)<\/p>/)[1];
+			const description = text.match(/<div class="uabb-infobox-text uabb-text-editor"><p>(.+)<\/p>/)[1]
+				.replace(/<a href="(.+)" target="_blank" rel="noopener">(.+)<\/a>/, '[$2]($1)')
+				.replace(/<em>(.+)<\/em>/i, '_$1_');
 			const embed = new MessageEmbed()
 				.setTitle(`${year}: ${time}`)
 				.setColor(0x000000)
 				.setURL('https://thebulletin.org/doomsday-clock/current-time/')
 				.setAuthor('Bulletin of the Atomic Scientists', undefined, 'https://thebulletin.org/')
-				.setDescription(description.replace(/<a href="(.+)" target="_blank" rel="noopener">(.+)<\/a>/, '[$2]($1)'));
+				.setDescription(description);
 			return msg.embed(embed);
 		} catch (err) {
 			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
