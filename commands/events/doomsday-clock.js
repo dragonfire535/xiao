d898a25d08c510aedbc40d93d9b1fe9ffa683979
Dragon Fire <danielbodendahl@gmail.com>
2020-01-25 22:30:16 -0500

    Fix doomsday clock

--- commands/events/doomsday-clock.js
+++ commands/events/doomsday-clock.js
@@ -25,7 +25,7 @@ module.exports = class DoomsdayClockCommand extends Command {
 			const { text } = await request.get('https://thebulletin.org/doomsday-clock/past-announcements/');
 			const time = text.match(/<h3 class="uabb-infobox-title">(.+)<\/h3>/)[1];
 			const year = text.match(/<h5 class="uabb-infobox-title-prefix">(.+)<\/h5>/)[1];
-			const description = text.match(/<div class="uabb-infobox-text uabb-text-editor"><p>(.+)<\/p>/)[1]
+			const description = text.match(/<div class="uabb-infobox-text uabb-text-editor">.+<p>(.+)<\/p>/)[1]
 				.replace(/<a href="(.+)" target="_blank" rel="noopener">(.+)<\/a>/, '[$2]($1)')
 				.replace(/<em>(.+)<\/em>/i, '_$1_');
 			const embed = new MessageEmbed()
