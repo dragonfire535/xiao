6439b773ca8368a52d9a60afd76a29d84f662b0f
Dragon Fire <danielbodendahl@gmail.com>
2020-02-28 12:02:24 -0500

    Correct dependency links for forks in info

--- commands/util/info.js
+++ commands/util/info.js
@@ -42,7 +42,7 @@ module.exports = class InfoCommand extends Command {
 		return Object.entries(dependencies).map(dep => {
 			if (dep[1].startsWith('github:')) {
 				const repo = dep[1].replace('github:', '').split('/');
-				return `[${dep[0]}](https://github.com/${repo[0]}/${repo[1].replace(/#.+/, '')})`;
+				return `[${dep[0]}](https://github.com/${repo[0]}/${repo[1].replace(/#(.+)/, '/tree/$1')})`;
 			}
 			return `[${dep[0]}](https://npmjs.com/${dep[0]})`;
 		}).join(', ');
