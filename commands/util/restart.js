d15deb5949f83b608d7f7e67048664d643b1ac37
Dragon Fire <danielbodendahl@gmail.com>
2020-03-06 17:49:32 -0500

    Restart Command

---
+++ commands/util/restart.js
@@ -0,0 +1,29 @@
+const Command = require('../../structures/Command');
+
+module.exports = class RestartCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'restart',
+			aliases: ['die', 'explode', 'shutdown', 'process.exit'],
+			group: 'util',
+			memberName: 'restart',
+			description: 'Restarts the bot.',
+			details: 'Only the bot owner(s) may use this command.',
+			guarded: true,
+			ownerOnly: true,
+			args: [
+				{
+					key: 'code',
+					prompt: 'What code do you want to send to `process.exit`?',
+					type: 'integer',
+					default: 0
+				}
+			]
+		});
+	}
+
+	run(msg, { code }) {
+		process.exit(code);
+		return null;
+	}
+};
