55baab21255c2035b558d5e089b71fcbec38663c
Dragon Fire <danielbodendahl@gmail.com>
2020-03-06 17:51:31 -0500

    Send a console log message when restart is used

--- commands/util/restart.js
+++ commands/util/restart.js
@@ -23,6 +23,7 @@ module.exports = class RestartCommand extends Command {
 	}
 
 	run(msg, { code }) {
+		client.logger.info('[RESTART] Manually restarted.');
 		process.exit(code);
 		return null;
 	}
