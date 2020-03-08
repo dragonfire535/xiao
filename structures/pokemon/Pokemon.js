0f3eb4939e41da725c6cd1a6d4c042bd6a4dbf74
Dragon Fire <danielbodendahl@gmail.com>
2020-03-08 11:16:26 -0400

    Remove Duplicates from Pokemon Pokedex entries

--- structures/pokemon/Pokemon.js
+++ structures/pokemon/Pokemon.js
@@ -1,10 +1,12 @@
+const { removeDuplicates } = require('../../util/Util');
+
 module.exports = class Pokemon {
 	constructor(data) {
 		this.id = data.id;
 		this.name = data.names.find(entry => entry.language.name === 'en').name;
-		this.entries = data.flavor_text_entries
+		this.entries = removeDuplicates(data.flavor_text_entries
 			.filter(entry => entry.language.name === 'en')
-			.map(entry => entry.flavor_text.replace(/\n|\f|\r/g, ' '));
+			.map(entry => entry.flavor_text.replace(/\n|\f|\r/g, ' ')));
 		this.names = data.names.map(entry => ({ name: entry.name, language: entry.language.name }));
 		this.genus = `The ${data.genera.filter(entry => entry.language.name === 'en')[0].genus}`;
 	}
