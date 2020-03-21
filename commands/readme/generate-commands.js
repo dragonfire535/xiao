17c7520d9b4d48368875029d654d19b874a6bea5
Dragon Fire <danielbodendahl@gmail.com>
2020-03-21 17:50:18 -0400

    Use txt files in README generator commands

--- commands/readme/generate-commands.js
+++ commands/readme/generate-commands.js
@@ -1,5 +1,4 @@
 const Command = require('../../structures/Command');
-const request = require('node-superfetch');
 
 module.exports = class GenerateCommandsCommand extends Command {
 	constructor(client) {
@@ -11,25 +10,15 @@ module.exports = class GenerateCommandsCommand extends Command {
 			description: 'Generates the commands list for Xiao\'s README.',
 			details: 'Only the bot owner(s) may use this command.',
 			ownerOnly: true,
-			guarded: true,
-			credit: [
-				{
-					name: 'Hastebin',
-					url: 'https://hastebin.com/about.md',
-					reason: 'API'
-				}
-			]
+			guarded: true
 		});
 	}
 
-	async run(msg) {
+	run(msg) {
 		const list = this.client.registry.groups
 			.map(g => `\n### ${g.name}:\n\n${g.commands.filter(c => !c.hidden).map(
 				c => `* **${c.name}:** ${c.description}${c.ownerOnly ? ' (Owner-Only)' : ''}${c.nsfw ? ' (NSFW)' : ''}`
 			).join('\n')}`);
-		const { body } = await request
-			.post('https://hastebin.com/documents')
-			.send(`Total: ${this.client.registry.commands.size}\n${list.join('\n')}`);
-		return msg.say(`https://hastebin.com/raw/${body.key}`);
+		return msg.channel.send({ files: [{ attachment: Buffer.from(list), name: 'commands.txt' }] });
 	}
 };
