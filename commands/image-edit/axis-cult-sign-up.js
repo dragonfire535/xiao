5cdb2a54cee5197981721579f18b18335139cc4d
Dragon Fire <danielbodendahl@gmail.com>
2020-03-25 10:26:19 -0400

    Fix huge axis cult sign up filesize

--- commands/image-edit/axis-cult-sign-up.js
+++ commands/image-edit/axis-cult-sign-up.js
@@ -72,7 +72,7 @@ module.exports = class AxisCultSignUpCommand extends Command {
 		ctx.fillText('Xiao', 960, 2370);
 		ctx.font = '123px Konosuba';
 		ctx.fillText('Eris pads\nher chest!', 1037, 3048);
-		return msg.say({ files: [{ attachment: canvas.toBuffer(), name: 'axis-cult-sign-up.png' }] });
+		return msg.say({ files: [{ attachment: canvas.toBuffer('image/jpeg'), name: 'axis-cult-sign-up.jpg' }] });
 	}
 };
 
