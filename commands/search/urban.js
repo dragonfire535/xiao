445741424f2c843509db87e0d9a3075258b4b63f
Dragon Fire <danielbodendahl@gmail.com>
2020-04-11 12:06:27 -0400

    Don't allow urban in non-nsfw channels

--- commands/search/urban.js
+++ commands/search/urban.js
@@ -31,6 +31,9 @@ module.exports = class UrbanCommand extends Command {
 	}
 
 	async run(msg, { word }) {
+		if (msg.guild && this.client.botListGuilds.includes(msg.guild.id) && !msg.channel.nsfw) {
+			return msg.reply(`The \`${this.name}\` command can only be used in NSFW channels.`);
+		}
 		try {
 			const { body } = await request
 				.get('http://api.urbandictionary.com/v0/define')
