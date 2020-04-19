e94f5d7e70e13a3b80cda77700c60b138e4edc51
Dragon Fire <danielbodendahl@gmail.com>
2020-04-19 17:15:27 -0400

    Police Tape Command

---
+++ commands/edit-image/police-tape.js
@@ -0,0 +1,56 @@
+const Command = require('../../structures/Command');
+const { createCanvas, loadImage } = require('canvas');
+const request = require('node-superfetch');
+const path = require('path');
+const { centerImage } = require('../../util/Canvas');
+
+module.exports = class PoliceTapeCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'police-tape',
+			aliases: ['caution-tape', 'caution'],
+			group: 'edit-image',
+			memberName: 'police tape',
+			description: 'Draws police tape over an image or a user\'s avatar.',
+			throttling: {
+				usages: 1,
+				duration: 10
+			},
+			clientPermissions: ['ATTACH_FILES'],
+			credit: [
+				{
+					name: 'PNG Arts',
+					url: 'https://www.pngarts.com/',
+					reason: 'Image',
+					reasonURL: 'https://www.pngarts.com/explore/94078'
+				}
+			],
+			args: [
+				{
+					key: 'image',
+					prompt: 'What image would you like to edit?',
+					type: 'image',
+					default: msg => msg.author.displayAvatarURL({ format: 'png', size: 512 })
+				}
+			]
+		});
+	}
+
+	async run(msg, { image }) {
+		try {
+			const base = await loadImage(path.join(__dirname, '..', '..', 'assets', 'images', 'police-tape.png'));
+			const { body } = await request.get(image);
+			const data = await loadImage(body);
+			const canvas = createCanvas(data.width, data.height);
+			const ctx = canvas.getContext('2d');
+			ctx.drawImage(data, 0, 0);
+			const { x, y, width, height } = centerImage(base, data);
+			ctx.drawImage(base, x, y, width, height);
+			const attachment = canvas.toBuffer();
+			if (Buffer.byteLength(attachment) > 8e+6) return msg.reply('Resulting image was above 8 MB.');
+			return msg.say({ files: [{ attachment, name: 'police-tape.png' }] });
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+};
