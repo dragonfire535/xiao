2bfd614518f922f3f16bc0c56ef34bc631c8dfe3
Dragon Fire <danielbodendahl@gmail.com>
2020-04-21 18:58:41 -0400

    Fix

--- commands/games-sp/horse-race.js
+++ commands/games-sp/horse-race.js
@@ -94,9 +94,9 @@ module.exports = class HorseRaceCommand extends Command {
 		for (let i = 0; i < horses.length; i++) {
 			const horse = horses[i];
 			const result = results.find(result => horse.name === result.name).time;
-			if (colors[i]) drawImageWithTint(ctx, horseImg, colors[i], 37, 114 * i, 49, 49);
-			ctx.fillText(this.formatTime(result), 138, 755 * i);
-			ctx.fillText(horse.name, 138, 251 * i);
+			if (colors[i]) drawImageWithTint(ctx, horseImg, colors[i], 37, 114 + (horseImg.height * i), 49, 49);
+			ctx.fillText(this.formatTime(result), 755, 138 + (49 * i));
+			ctx.fillText(horse.name, 755, 251 + (49 * i));
 		}
 		return { attachment: canvas.toBuffer(), name: 'leaderboard.png' };
 	}
