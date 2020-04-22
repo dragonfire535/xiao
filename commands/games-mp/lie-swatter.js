770b97561af1d0715b59b3da9425961587456a63
Dragon Fire <danielbodendahl@gmail.com>
2020-04-21 23:05:48 -0400

    Fix lint

--- commands/games-mp/lie-swatter.js
+++ commands/games-mp/lie-swatter.js
@@ -72,7 +72,7 @@ module.exports = class LieSwatterCommand extends Command {
 					if (trueOptions.includes(answer) || falseOptions.includes(answer)) {
 						res.react(SUCCESS_EMOJI_ID || '✅').catch(() => null);
 						return true;
-					};
+					}
 					return false;
 				};
 				const msgs = await msg.channel.awaitMessages(filter, {
