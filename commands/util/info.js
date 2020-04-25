2fde143c6b811457ddef865ad4cf6493e8a37a78
Dragon Fire <danielbodendahl@gmail.com>
2020-04-24 21:36:41 -0400

    Make dependencies take up less space

--- commands/util/info.js
+++ commands/util/info.js
@@ -52,6 +52,6 @@ module.exports = class InfoCommand extends Command {
 				return `[${dep[0]}](https://github.com/${repo[0]}/${repo[1].replace(/#(.+)/, '/tree/$1')})`;
 			}
 			return `[${dep[0]}](https://npmjs.com/${dep[0]})`;
-		}).join(', ');
+		}).join(' ');
 	}
 };
