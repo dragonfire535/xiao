a5948cc69956d9cb76f8312d5c86664297c88e99
Dragon Fire <danielbodendahl@gmail.com>
2020-04-25 22:34:45 -0400

    Inhale Command

---
+++ commands/roleplay/inhale.js
@@ -0,0 +1,26 @@
+const ImgurAlbumCommand = require('../../structures/commands/ImgurAlbum');
+const { INHALE_ALBUM_ID } = process.env;
+
+module.exports = class InhaleCommand extends ImgurAlbumCommand {
+	constructor(client) {
+		super(client, {
+			name: 'inhale',
+			group: 'roleplay',
+			memberName: 'inhale',
+			description: 'Inhales a user.',
+			clientPermissions: ['ATTACH_FILES'],
+			albumID: INHALE_ALBUM_ID,
+			args: [
+				{
+					key: 'user',
+					prompt: 'What user do you want to roleplay with?',
+					type: 'user'
+				}
+			]
+		});
+	}
+
+	generateText(msg, user) {
+		return `_**${msg.author.username}** inhales **${user.username}** but gained no ability._`;
+	}
+};
