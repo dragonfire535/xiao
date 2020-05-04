cb3d494cc4822e9913cc08e14abf1fcc61e30062
Dragon Fire <danielbodendahl@gmail.com>
2020-05-04 11:22:36 -0400

    Remove text on fly by default

--- commands/single/fly.js
+++ commands/single/fly.js
@@ -1,7 +1,5 @@
 const Command = require('../../structures/Command');
 const path = require('path');
-const { list } = require('../../util/Util');
-const types = ['default', 'blank'];
 
 module.exports = class FlyCommand extends Command {
 	constructor(client) {
@@ -10,22 +8,11 @@ module.exports = class FlyCommand extends Command {
 			group: 'single',
 			memberName: 'fly',
 			description: 'Sends a fake fly that looks surprisngly real.',
-			details: `**Types:** ${types.join(', ')}`,
-			clientPermissions: ['ATTACH_FILES'],
-			args: [
-				{
-					key: 'type',
-					prompt: `What type of meme do you want to use? Either ${list(types, 'or')}.`,
-					type: 'string',
-					default: 'default',
-					oneOf: types,
-					parse: type => type.toLowerCase()
-				}
-			]
+			clientPermissions: ['ATTACH_FILES']
 		});
 	}
 
-	run(msg, { type }) {
-		return msg.say({ files: [path.join(__dirname, '..', '..', 'assets', 'images', 'fly', `${type}.png`)] });
+	run(msg) {
+		return msg.say({ files: [path.join(__dirname, '..', '..', 'assets', 'images', 'fly.png')] });
 	}
 };
