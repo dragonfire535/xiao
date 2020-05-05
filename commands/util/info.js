82b58e4504e5abbb1e0983ae892aa63726f5181d
Dragon Fire <danielbodendahl@gmail.com>
2020-05-05 17:47:23 -0400

    Make info embed deps only use npm

--- commands/util/info.js
+++ commands/util/info.js
@@ -46,12 +46,6 @@ module.exports = class InfoCommand extends Command {
 	}
 
 	parseDependencies(deps) {
-		return Object.entries(deps).map(dep => {
-			if (dep[1].startsWith('github:')) {
-				const repo = dep[1].replace('github:', '').split('/');
-				return `[${dep[0]}](https://github.com/${repo[0]}/${repo[1].replace(/#(.+)/, '/tree/$1')})`;
-			}
-			return `[${dep[0]}](https://npmjs.com/${dep[0]})`;
-		}).join(' ');
+		return Object.keys(deps).map(dep => `[${dep}](https://npmjs.com/${dep})`).join(', ');
 	}
 };
