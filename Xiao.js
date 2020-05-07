b392a1384304c28edfb28b4b7de838da2735292f
Dragon Fire <danielbodendahl@gmail.com>
2020-05-06 21:07:50 -0400

    Fix

--- Xiao.js
+++ Xiao.js
@@ -69,7 +69,7 @@ client.on('message', async msg => {
 	const hasText = Boolean(msg.content);
 	const hasImage = msg.attachments.size !== 0;
 	const hasEmbed = msg.embeds.length !== 0;
-	if (!hasText && !hasImage && !hasEmbed) return;
+	if (msg.author.bot || (!hasText && !hasImage && !hasEmbed)) return;
 	const origin = client.phone.find(call => call.origin.id === msg.channel.id);
 	const recipient = client.phone.find(call => call.recipient.id === msg.channel.id);
 	if (!origin && !recipient) return;
