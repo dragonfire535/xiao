dd8b8f82bf91a5f49d18761d5941354b6d33305f
Dragon Fire <danielbodendahl@gmail.com>
2020-05-07 22:34:10 -0400

    Make shortened text in canvas take into account the ...

--- util/Canvas.js
+++ util/Canvas.js
@@ -106,7 +106,7 @@ module.exports = class CanvasUtil {
 
 	static shortenText(ctx, text, maxWidth) {
 		let shorten = false;
-		while (ctx.measureText(text).width > maxWidth) {
+		while (ctx.measureText(`${text}...`).width > maxWidth) {
 			if (!shorten) shorten = true;
 			text = text.substr(0, text.length - 1);
 		}
