2ef5ae393867d8e41b5a7902db4fc67a9acd000c
Overtime <39917164+Overtime2005@users.noreply.github.com>
2020-05-23 11:01:20 -0400

    added gif.js

---
+++ commands/random-img/gif.js
@@ -0,0 +1,27 @@
+const ImgurAlbumCommand = require('../../structures/commands/ImgurAlbum');
+const { GIF_ALBUM_ID } = process.env;
+
+module.exports = class GifCommand extends ImgurAlbumCommand {
+	constructor(client) {
+		super(client, {
+			name: 'gif',
+			aliases: ['gif'],
+			group: 'random-img',
+			memberName: 'gif',
+			description: 'Responds with a random gif.',
+			clientPermissions: ['ATTACH_FILES'],
+			albumID: GIF_ALBUM_ID,
+			credit: [
+				{
+					name: 'overtime2005',
+					url: 'https://github.com/Overtime2005',
+					reason: 'concept/code'
+				}
+			]
+		});
+	}
+
+	generateText() {
+		return 'heres your gif master';
+	}
+};
