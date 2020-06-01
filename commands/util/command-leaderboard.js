dc7923f0e9ca6186cfff49c24291b90b3cf38424
Dragon Fire <danielbodendahl@gmail.com>
2020-05-31 22:02:43 -0400

    Add Page Marker to Cmdlb

--- commands/util/command-leaderboard.js
+++ commands/util/command-leaderboard.js
@@ -23,9 +23,11 @@ module.exports = class CommandLeaderboardCommand extends Command {
 	}
 
 	run(msg, { page }) {
+		const lb = this.makeLeaderboard(page);
+		if (!lb.length) return msg.say(`Page ${page} does not exist (yet).`);
 		return msg.say(stripIndents`
-			__**Command Usage Leaderboard (as of Last Restart):**__
-			${this.makeLeaderboard(page).join('\n')}
+			__**Command Usage Leaderboard (Page ${page}):**__
+			${lb.join('\n')}
 		`);
 	}
 
