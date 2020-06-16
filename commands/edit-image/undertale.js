54a08056ce0a57e527ad551116500a94af060d1c
Dragon Fire <danielbodendahl@gmail.com>
2020-06-15 20:55:27 -0400

    Fix

--- commands/edit-image/undertale.js
+++ commands/edit-image/undertale.js
@@ -101,28 +101,28 @@ module.exports = class UndertaleCommand extends Command {
 		const ctx = canvas.getContext('2d');
 		ctx.drawImage(base, 0, 0);
 		let font = 'DeterminationMono';
-		let space = 12;
+		let space = 6;
 		switch (character) {
 			case 'sans':
 				font = 'UndertaleSans';
 				quote = quote.toLowerCase();
-				space = 10;
+				space = 5;
 				break;
 			case 'papyrus':
 				font = 'UndertalePapyrus';
 				quote = quote.toUpperCase();
-				space = 8;
+				space = 4;
 				break;
 			case 'napstablook':
 				quote = quote.toLowerCase();
 				break;
 			case 'gaster':
 				font = 'Pixelated Wingdings';
-				space = 10;
+				space = 5;
 				break;
 			case 'ness':
 				font = 'Apple Kid';
-				space = 14;
+				space = 7;
 				break;
 			case 'temmie':
 				quote = this.client.registry.commands.get('temmie').temmize(quote);
