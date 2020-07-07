9b62b1f20d7751aae58cb307e3233574f8f1c76f
Dragon Fire <danielbodendahl@gmail.com>
2020-07-07 10:49:11 -0400

    Revert

--- structures/phone/PhoneCall.js
+++ structures/phone/PhoneCall.js
@@ -36,10 +36,8 @@ module.exports = class PhoneCall {
 		this.active = true;
 		this.setTimeout();
 		if (this.ownerOrigin) return this;
-		const inGuild = msg.guild ? undefined : null;
-		const cmd = msg.anyUsage('hang-up', inGuild, inGuild);
-		await this.origin.send(`☎️ **${this.recipient.guild.name}** picked up! Use ${cmd} hang up.`);
-		await this.recipient.send(`☎️ Accepted call from **${this.origin.guild.name}**. Use ${cmd} to hang up.`);
+		await this.origin.send(`☎️ **${this.recipient.guild.name}** picked up! Type \`hang up\` to hang up.`);
+		await this.recipient.send(`☎️ Accepted call from **${this.origin.guild.name}**. Type \`hang up\` to hang up.`);
 		return this;
 	}
 
