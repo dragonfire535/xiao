d6a269496951e1f018a8e4d7a3b3a53d488d86f6
Dragon Fire <danielbodendahl@gmail.com>
2020-07-13 20:26:57 -0400

    Metamorphosis Command

---
+++ commands/edit-meme/metamorphosis.js
@@ -0,0 +1,77 @@
+const Command = require('../../structures/Command');
+const { createCanvas, loadImage, registerFont } = require('canvas');
+const request = require('node-superfetch');
+const path = require('path');
+const { centerImagePart } = require('../../util/Canvas');
+registerFont(path.join(__dirname, '..', '..', 'assets', 'fonts', 'Noto-Regular.ttf'), { family: 'Noto' });
+registerFont(path.join(__dirname, '..', '..', 'assets', 'fonts', 'Noto-CJK.otf'), { family: 'Noto' });
+registerFont(path.join(__dirname, '..', '..', 'assets', 'fonts', 'Noto-Emoji.ttf'), { family: 'Noto' });
+
+module.exports = class MetamorphosisCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'metamorphosis',
+			aliases: ['my-metamorphosis-begins', 'morph'],
+			group: 'edit-meme',
+			memberName: 'metamorphosis',
+			description: 'Sends a "My Metamorphosis Begins" meme with the image and text of your choice.',
+			throttling: {
+				usages: 1,
+				duration: 10
+			},
+			clientPermissions: ['ATTACH_FILES'],
+			credit: [
+				{
+					name: 'Google',
+					url: 'https://www.google.com/',
+					reason: 'Noto Font',
+					reasonURL: 'https://www.google.com/get/noto/'
+				},
+				{
+					name: 'LuckyAquapura',
+					url: 'https://twitter.com/alcremimiga',
+					reason: 'Concept'
+				},
+				{
+					name: 'Yeah I\'m Stuck in the Void, Keep Scrolling',
+					url: 'https://www.facebook.com/voidmanthing/',
+					reason: 'Image',
+					reasonURL: 'https://www.facebook.com/voidmanthing/posts/125724262420450'
+				}
+			],
+			args: [
+				{
+					key: 'name',
+					prompt: 'What is the name of thing to morph into?',
+					type: 'string',
+					max: 280
+				},
+				{
+					key: 'image',
+					prompt: 'What image would you like to edit?',
+					type: 'image',
+					default: msg => msg.author.displayAvatarURL({ format: 'png', size: 256 })
+				}
+			]
+		});
+	}
+
+	async run(msg, { name, image }) {
+		try {
+			const base = await loadImage(path.join(__dirname, '..', '..', 'assets', 'images', 'metamorphosis.png'));
+			const { body } = await request.get(image);
+			const data = await loadImage(body);
+			const canvas = createCanvas(base.width, base.height);
+			const ctx = canvas.getContext('2d');
+			ctx.drawImage(base, 0, 0);
+			const { x, y, width, height } = centerImagePart(data, 200, 200, 412, 257);
+			ctx.drawImage(data, x, y, width, height);
+			ctx.textBaseline = 'top';
+			ctx.font = '20px Noto';
+			ctx.fillText(`le ${name.toLowerCase()}`, 345, 466, 330);
+			return msg.say({ files: [{ attachment: canvas.toBuffer(), name: 'metamorphosis.png' }] });
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+};
