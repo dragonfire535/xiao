4d340fea9227cc1540c0e1c1bec9e0910cd2d00e
Dragon Fire <danielbodendahl@gmail.com>
2020-08-25 20:40:30 -0400

    Fix

--- commands/analyze/face.js
+++ commands/analyze/face.js
@@ -63,7 +63,8 @@ module.exports = class FaceCommand extends Command {
 		if (Buffer.byteLength(imgData.body) >= 2e+6) return 'size';
 		const { body } = await request
 			.post('https://api-us.faceplusplus.com/facepp/v3/detect')
-			.attach('image_file', imgData.body)
+			.headers({ 'Content-Type': 'multipart/form-data' })
+			.attach({ image_file: imgData.body })
 			.query({
 				api_key: FACEPLUSPLUS_KEY,
 				api_secret: FACEPLUSPLUS_SECRET,
