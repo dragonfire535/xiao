32917c386c2e7fcf10a93476be46122568293194
Dragon Fire <danielbodendahl@gmail.com>
2020-11-04 13:20:10 -0500

    Fix

--- commands/edit-meme/illegal.js
+++ commands/edit-meme/illegal.js
@@ -80,9 +80,9 @@ module.exports = class IllegalCommand extends Command {
 			ctx.textBaseline = 'top';
 			ctx.font = '20px Impact';
 			const rect = {
-				x: frame.corners[1][0],
-				y: frame.corners[0][0],
-				width: frame.corners[0][1],
+				x: frame.corners[0][0],
+				y: frame.corners[0][1],
+				width: frame.corners[1][0],
 				height: frame.corners[0][0]
 			};
 			drawText(ctx, `${text}\n${verb} NOW\nILLEGAL`, impactFont, rect, {
