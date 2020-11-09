f1397894963aceeb153adcdd72f0bf9adb0fce5c
Dragon Fire <danielbodendahl@gmail.com>
2020-11-09 15:39:51 -0500

    Import Command Leaderboard after reload

--- commands/util/reload.js
+++ commands/util/reload.js
@@ -7,25 +7,24 @@ module.exports = class ReloadCommand extends Command {
 			aliases: ['reload-command', 'reload-cmd'],
 			group: 'util',
 			memberName: 'reload',
-			description: 'Reloads a command or command group.',
+			description: 'Reloads a command.',
 			details: 'Only the bot owner(s) may use this command.',
 			guarded: true,
 			ownerOnly: true,
 			args: [
 				{
-					key: 'cmdOrGrp',
-					label: 'command/group',
-					prompt: 'Which command or group would you like to reload?',
-					type: 'command|group'
+					key: 'command',
+					label: 'command',
+					prompt: 'Which command would you like to reload?',
+					type: 'command'
 				}
 			]
 		});
 	}
 
-	run(msg, { cmdOrGrp }) {
-		const isCmd = Boolean(cmdOrGrp.groupID);
-		cmdOrGrp.reload();
-		if (isCmd) return msg.say(`Reloaded the \`${cmdOrGrp.name}\` command.`);
-		return msg.say(`Reloaded all of the commands in the \`${cmdOrGrp.name}\` group.`);
+	run(msg, { command }) {
+		command.reload();
+		this.client.importCommandLeaderboard();
+		return msg.say(`Reloaded the \`${command.name}\` command.`);
 	}
 };
