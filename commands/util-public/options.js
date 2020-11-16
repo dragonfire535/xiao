480e736b53715428448cf334042416c998db882d
Dragon Fire <danielbodendahl@gmail.com>
2020-11-16 16:43:51 -0500

    Make Options Nicer

--- commands/util-public/options.js
+++ commands/util-public/options.js
@@ -18,12 +18,17 @@ module.exports = class OptionsCommand extends Command {
 			__**Server Options**__
 			Place the option in the appropriate channel's topic to use.
 
+			__General Options__
 			\`<xiao:disable-leave>\` Disables leave messages (System Channel).
+
+			__Phone Options__
 			\`<xiao:phone>\` Allows this channel to recieve phone calls.
 			\`<xiao:phone:no-voicemail>\` Prevents this channel from recieving voicemails for missed calls.
 			\`<xiao:phone:no-random>\` Makes the channel only able to be called directly, rather than picked randomly.
 			\`<xiao:phone:block:INSERTIDHERE>\` Blocks a channel or server from contacting you via phone.
 			\`<xiao:phone-book:hide>\` Hides this channel from \`phone-book\`.
+
+			__Portal Options__
 			\`<xiao:portal>\` Marks the channel as a portal channel for \`portal-send\`.
 			\`<xiao:portal:hide-name>\` Hides the channel's name when the channel is chosen to recieve a portal message.
 		`);
