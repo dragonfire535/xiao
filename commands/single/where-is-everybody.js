390853e2083f66b7513e6218eae9b4d78be9a93a
Dragon Fire <danielbodendahl@gmail.com>
2020-12-03 14:28:37 -0500

    Add quotes

--- commands/single/where-is-everybody.js
+++ commands/single/where-is-everybody.js
@@ -28,15 +28,15 @@ module.exports = class WhereIsEverybodyCommand extends Command {
 	}
 
 	async run(msg) {
-		await msg.channel.send('It\'s quiet...', {
+		await msg.channel.send('"It\'s quiet..."', {
 			files: [path.join(__dirname, '..', '..', 'assets', 'images', 'where-is-everybody', 'part-1.jpg')]
 		});
 		await delay(5000);
-		await msg.channel.send('Too quiet...', {
+		await msg.channel.send('"Too quiet..."', {
 			files: [path.join(__dirname, '..', '..', 'assets', 'images', 'where-is-everybody', 'part-2.jpg')]
 		});
 		await delay(5000);
-		return msg.channel.send('Where is @everyone?', {
+		return msg.channel.send('"Where is @everyone?"', {
 			disableMentions: 'none',
 			files: [path.join(__dirname, '..', '..', 'assets', 'images', 'where-is-everybody', 'part-3.jpg')]
 		});
