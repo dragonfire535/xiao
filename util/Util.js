8c180ad5584a6180b7706640729d38241f93b6e5
Dragon Fire <danielbodendahl@gmail.com>
2021-01-16 13:11:08 -0500

    Fix lint

--- util/Util.js
+++ util/Util.js
@@ -178,7 +178,7 @@ module.exports = class Util {
 
 	static rgbToHex(r, g, b) {
 		if (r > 255 || g > 255 || b > 255) return null;
-    	return ((r << 16) | (g << 8) | b).toString(16);
+		return ((r << 16) | (g << 8) | b).toString(16);
 	}
 
 	static magikToBuffer(magik) {
