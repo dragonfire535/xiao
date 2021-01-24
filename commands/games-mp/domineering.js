f7ccc994cc66724ef802dd17db473e2c335ca2cc
Dragon Fire <danielbodendahl@gmail.com>
2021-01-24 10:09:52 -0500

    Fix

--- commands/games-mp/domineering.js
+++ commands/games-mp/domineering.js
@@ -119,7 +119,8 @@ module.exports = class DomineeringCommand extends Command {
 		for (let i = 0; i < board.length; i++) {
 			for (let j = 0; j < board[i].length; j++) {
 				if (board[i][j]) continue;
-				if (!board[i + 1] || !board[i][j + 1]) continue;
+				if (userTurn && !board[i + 1]) continue;
+				if (!userTurn && !board[i][j + 1]) continue;
 				if (board[userTurn ? i + 1 : i][userTurn ? j : j + 1]) continue;
 				possibleMoves.push(`${i},${j}`);
 			}
