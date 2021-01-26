04af24790fc1bcac7db586b5217bcd415b96284b
Dragon Fire <danielbodendahl@gmail.com>
2021-01-26 16:37:11 -0500

    Optimize Prime Check

--- commands/edit-number/prime.js
+++ commands/edit-number/prime.js
@@ -26,8 +26,8 @@ module.exports = class PrimeCommand extends Command {
 
 	isPrime(number) {
 		if (number < 2) return false;
-		for (let i = 2; i < number; i++) {
-			if (number % i === 0) return false;
+		for (let i = 2, s = Math.sqrt(number); i <= s; i++) {
+			if (number % i === 0) return false; 
 		}
 		return true;
 	}
