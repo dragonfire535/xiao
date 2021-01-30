db242f6c924cc662a92b0ea850d3b772b7c46088
Dragon Fire <danielbodendahl@gmail.com>
2021-01-29 20:54:06 -0500

    Motion Blur Command

---
+++ commands/edit-image/motion-blur.js
@@ -0,0 +1,44 @@
+const Command = require('../../structures/Command');
+const { createCanvas, loadImage } = require('canvas');
+const request = require('node-superfetch');
+const { motionBlur } = require('../../util/Canvas');
+
+module.exports = class MotionBlurCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'motion-blur',
+			aliases: ['m-blur', 'motion'],
+			group: 'edit-image',
+			memberName: 'motion-blur',
+			description: 'Draws an image or a user\'s avatar with motion blur.',
+			throttling: {
+				usages: 1,
+				duration: 10
+			},
+			clientPermissions: ['ATTACH_FILES'],
+			args: [
+				{
+					key: 'image',
+					prompt: 'What image would you like to edit?',
+					type: 'image',
+					default: msg => msg.author.displayAvatarURL({ format: 'png', size: 512 })
+				}
+			]
+		});
+	}
+
+	async run(msg, { image }) {
+		try {
+			const { body } = await request.get(image);
+			const data = await loadImage(body);
+			const canvas = createCanvas(data.width, data.height);
+			const ctx = canvas.getContext('2d');
+			motionBlur(ctx, data, 0, 0, data.width, data.height);
+			const attachment = canvas.toBuffer();
+			if (Buffer.byteLength(attachment) > 8e+6) return msg.reply('Resulting image was above 8 MB.');
+			return msg.say({ files: [{ attachment, name: 'motion-blur.png' }] });
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+};
