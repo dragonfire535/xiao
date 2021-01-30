41e58549fb0b69c941abc4bc6fb394cc0663ae7a
Dragon Fire <danielbodendahl@gmail.com>
2021-01-30 16:30:13 -0500

    Update to latest commando

--- types/image-or-avatar.js
+++ types/image-or-avatar.js
@@ -5,21 +5,21 @@ module.exports = class ImageOrAvatarArgumentType extends ArgumentType {
 		super(client, 'image-or-avatar');
 	}
 
-	async validate(value, msg, arg) {
-		const image = await this.client.registry.types.get('image').validate(value, msg, arg);
+	async validate(value, msg, arg, currentMsg) {
+		const image = await this.client.registry.types.get('image').validate(value, msg, arg, currentMsg);
 		if (image) return image;
-		return this.client.registry.types.get('user').validate(value, msg, arg);
+		return this.client.registry.types.get('user').validate(value, msg, arg, currentMsg);
 	}
 
 	async parse(value, msg, arg) {
-		const image = this.client.registry.types.get('image').parse(value, msg, arg);
+		const image = this.client.registry.types.get('image').parse(value, msg, arg, currentMsg);
 		if (image) return image;
-		const user = await this.client.registry.types.get('user').parse(value, msg, arg);
+		const user = await this.client.registry.types.get('user').parse(value, msg, arg, currentMsg);
 		return user.displayAvatarURL({ format: 'png', size: 512 });
 	}
 
 	isEmpty(value, msg, arg) {
-		return this.client.registry.types.get('image').isEmpty(value, msg, arg)
-			&& this.client.registry.types.get('user').isEmpty(value, msg, arg);
+		return this.client.registry.types.get('image').isEmpty(value, msg, arg, currentMsg)
+			&& this.client.registry.types.get('user').isEmpty(value, msg, arg, currentMsg);
 	}
 };
