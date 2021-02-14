46aaa1a00def0cbabf6f1b75e3c99fdbc8e9ac86
Dragon Fire <danielbodendahl@gmail.com>
2021-02-14 09:59:43 -0500

    Leave all blacklisted guilds when blacklist is used

--- commands/util/blacklist.js
+++ commands/util/blacklist.js
@@ -33,6 +33,30 @@ module.exports = class BlacklistCommand extends Command {
 		if (this.client.blacklist[type].includes(target)) return msg.say(`🔨 \`${target}\` is already blacklisted.`);
 		this.client.blacklist[type].push(target);
 		this.client.exportBlacklist();
+		if (type === 'guild') {
+			try {
+				const guild = await this.client.guilds.fetch(target, false);
+				await guild.leave();
+			} catch {
+				await msg.say('🔨 Failed to leave guild.');
+			}
+		}
+		if (type === 'user') {
+			let guildsLeft = 0;
+			const failedToLeave = [];
+			for (const guild of this.client.guilds.cache.values()) {
+				if (guild.ownerID === target) {
+					try {
+						await guild.leave();
+						guildsLeft++;
+					} catch {
+						failedToLeave.push(guild.id);
+					}
+				}
+			}
+			const formatFailed = failedToLeave.length ? failedToLeave.map(id => `\`${id}\``).join(', ') : '_None_';
+			await msg.say(`🔨 Left ${guildsLeft} guilds owner by this user. Failed to leave: ${formatFailed}`);
+		}
 		return msg.say(`🔨 Blacklisted ${type} \`${target}\`.`);
 	}
 };
