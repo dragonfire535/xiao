0bc4172802d8b3f77edbee1f7e27449dbc978b15
Dragon Fire <danielbodendahl@gmail.com>
2021-02-20 11:29:53 -0500

    Fix lint

--- types/font.js
+++ types/font.js
@@ -23,14 +23,14 @@ module.exports = class FontArgumentType extends ArgumentType {
 		});
 		if (foundExact.size === 1) return true;
 		if (foundExact.size > 0) found = foundExact;
-		return found.size <= 15 ?
-			`${disambiguation(found.map(font => escapeMarkdown(font.name)), 'fonts', null)}\n` :
-			'Multiple fonts found. Please be more specific.';
+		return found.size <= 15
+			? `${disambiguation(found.map(font => escapeMarkdown(font.name)), 'fonts', null)}\n`
+			: 'Multiple fonts found. Please be more specific.';
 	}
 
 	parse(value) {
 		const choice = value.toLowerCase();
-		let found = this.client.fonts.filter(font => {
+		const found = this.client.fonts.filter(font => {
 			if (font.isVariant) return false;
 			if (font.name.toLowerCase().includes(choice)) return true;
 			if (font.filenameNoExt.toLowerCase().includes(choice)) return true;
