9055666520824b08edebe13775629961ada85eeb
Dragon Fire <danielbodendahl@gmail.com>
2021-02-20 15:13:05 -0500

    You must be in the voice channel to remove the bot

--- commands/util-voice/leave.js
+++ commands/util-voice/leave.js
@@ -16,6 +16,9 @@ module.exports = class LeaveCommand extends Command {
 	run(msg) {
 		const connection = this.client.voice.connections.get(msg.guild.id);
 		if (!connection) return msg.reply('I am not in a voice channel.');
+		if (!connection.channel.members.has(msg.author.id)) {
+			return msg.reply('You must be in the voice channel to remove me from it.');
+		}
 		if (!msg.channel.permissionsFor(msg.author).has('MOVE_MEMBERS') && connection.channel.members.size > 2) {
 			return msg.reply('You need the "Move members" permission to remove me from this voice channel.');
 		}
