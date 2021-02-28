d8dfefbd0811e0ba027738291924c660db8243be
Dragon Fire <danielbodendahl@gmail.com>
2021-02-28 15:11:03 -0500

    Format Number Command

---
+++ commands/edit-number/format-number.js
@@ -0,0 +1,25 @@
+const Command = require('../../structures/Command');
+const { formatNumber } = require('../../util/Util');
+
+module.exports = class FormatNumberCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'format-number',
+			aliases: ['format-num', 'pretty-number', 'pretty-num'],
+			group: 'edit-number',
+			memberName: 'format-number',
+			description: 'Formats a number to look more readable.',
+			args: [
+				{
+					key: 'number',
+					prompt: 'What number do you want to format?',
+					type: 'float'
+				}
+			]
+		});
+	}
+
+	run(msg, { number }) {
+		return msg.reply(formatNumber(number));
+	}
+};
