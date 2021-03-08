a833c434287e450554d2df958ec1e6d026725ee3
Dragon Fire <danielbodendahl@gmail.com>
2021-03-08 17:02:02 -0500

    URL type

---
+++ types/url.js
@@ -0,0 +1,17 @@
+const { ArgumentType } = require('discord.js-commando');
+const { URL } = require('url');
+const validURL = require('valid-url');
+
+module.exports = class UrlType extends ArgumentType {
+	constructor(client) {
+		super(client, 'url');
+	}
+
+	validate(value) {
+		return Boolean(validURL.isWebUri(value));
+	}
+
+	async parse(value) {
+		return new URL(value);
+	}
+};
