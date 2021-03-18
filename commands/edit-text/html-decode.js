1d081c7701262c6f5769887867cde93d190cfac1
Dragon Fire <danielbodendahl@gmail.com>
2021-03-18 16:58:07 -0400

    Decode HTML Command

---
+++ commands/edit-text/html-decode.js
@@ -0,0 +1,29 @@
+const Command = require('../../structures/Command');
+const { decode: decodeHTML } = require('html-entities');
+
+module.exports = class HtmlDecodeCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'html-decode',
+			aliases: ['decode-html'],
+			group: 'edit-text',
+			memberName: 'html-decode',
+			description: 'Decodes HTML characters to regular characters.',
+			args: [
+				{
+					key: 'text',
+					prompt: 'What text would you like to decode?',
+					type: 'string',
+					validate: text => {
+						if (decodeHTML(text).length < 2000) return true;
+						return 'Invalid text, your text is too long.';
+					}
+				}
+			]
+		});
+	}
+
+	run(msg, { text }) {
+		return msg.say(decodeHTML(text));
+	}
+};
