9085a651ea576b4deaa46af56923adfa92726e74
Dragon Fire <danielbodendahl@gmail.com>
2021-03-21 11:35:15 -0400

    Filter stuff out of generate-commands

--- commands/util/generate-commands.js
+++ commands/util/generate-commands.js
@@ -7,7 +7,7 @@ module.exports = class GenerateCommandsCommand extends Command {
 			aliases: ['gen-commands', 'generate-cmds', 'gen-cmds'],
 			group: 'util',
 			memberName: 'generate-commands',
-			description: 'Generates the commands list for Xiao\'s README.',
+			description: 'Generates the commands list as a TXT file.',
 			details: 'Only the bot owner(s) may use this command.',
 			ownerOnly: true,
 			guarded: true
@@ -17,12 +17,11 @@ module.exports = class GenerateCommandsCommand extends Command {
 	async run(msg) {
 		const list = this.client.registry.groups
 			.map(g => {
-				const commands = g.commands.filter(c => !c.hidden);
-				return `\n### ${g.name}:\n\n${commands.map(c => {
-					const extra = `${c.ownerOnly ? ' (Owner-Only)' : ''}${c.nsfw ? ' (NSFW)' : ''}`;
-					return `* **${c.name}:** ${c.description}${extra}`;
-				}).join('\n')}`;
-			});
+				const commands = g.commands.filter(c => !c.hidden && !c.ownerOnly && !c.nsfw);
+				if (!commands.size) return null;
+				return `\n### ${g.name}:\n\n${commands.map(c => `* **${c.name}:** ${c.description}`).join('\n')}`;
+			})
+			.filter(cmds => cmds);
 		const text = `Total: ${this.client.registry.commands.size}\n${list.join('\n')}`;
 		await msg.direct({ files: [{ attachment: Buffer.from(text), name: 'commands.txt' }] });
 		return msg.say('📬 Sent `commands.txt` to your DMs!');
