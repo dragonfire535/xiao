4b8565413afaddb80bbb386cd32eb5331630db2d
Dragon Fire <danielbodendahl@gmail.com>
2021-03-21 10:44:30 -0400

    Ascii Command

---
+++ commands/edit-image/ascii.js
@@ -0,0 +1,54 @@
+const Command = require('../../structures/Command');
+const { loadImage } = require('canvas');
+const imageToAscii = require('image-to-ascii');
+const request = require('node-superfetch');
+
+module.exports = class AsciiCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'ascii',
+			group: 'edit-image',
+			memberName: 'ascii',
+			description: 'Draws an image or a user\'s avatar but with ascii.',
+			throttling: {
+				usages: 1,
+				duration: 15
+			},
+			args: [
+				{
+					key: 'image',
+					prompt: 'What image would you like to edit?',
+					type: 'image-or-avatar',
+					default: msg => msg.author.displayAvatarURL({ format: 'png', size: 512 })
+				}
+			]
+		});
+	}
+
+	async run(msg, { image }) {
+		try {
+			const { body } = await request.get(image);
+			const asciiImg = await this.ascii(body);
+			return msg.code(null, asciiImg);
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+
+	async ascii(image) {
+		const { width, height } = await loadImage(image);
+		const options = {
+			colored: false,
+			size: {
+				height: height >= width ? 25 : undefined,
+				width: width > height ? 25 : undefined
+			}
+		};
+		return new Promise((res, rej) => {
+			imageToAscii(image, options, (err, converted) => {
+				if (err) return rej(err);
+				return res(converted);
+			});
+		});
+	}
+};
