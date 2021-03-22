498149743e583cac3cab9a57ca47727830929f5a
Dragon Fire <danielbodendahl@gmail.com>
2021-03-22 17:53:04 -0400

    Graph model?

--- structures/Client.js
+++ structures/Client.js
@@ -188,7 +188,8 @@ module.exports = class XiaoClient extends CommandoClient {
 
 	async loadNSFWModel() {
 		const model = await nsfw.load(
-			url.pathToFileURL(path.join(__dirname, '..', 'tf_models', 'nsfw', 'web_model', 'model.json'))
+			url.pathToFileURL(path.join(__dirname, '..', 'tf_models', 'nsfw', 'web_model', 'model.json')),
+			{ type: 'graph' }
 		);
 		this.nsfwModel = model;
 		return this.nsfwModel;
