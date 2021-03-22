76f9495180bc8fe74b55ba51ed0d164ea16a703b
Dragon Fire <danielbodendahl@gmail.com>
2021-03-22 17:57:43 -0400

    Fix

--- structures/Client.js
+++ structures/Client.js
@@ -188,7 +188,7 @@ module.exports = class XiaoClient extends CommandoClient {
 
 	async loadNSFWModel() {
 		const model = await nsfw.load(
-			url.pathToFileURL(path.join(__dirname, '..', 'tf_models', 'nsfw', 'web_model', 'model.json')).href,
+			url.pathToFileURL(path.join(__dirname, '..', 'tf_models', 'nsfw', 'web_model')).href,
 			{ type: 'graph' }
 		);
 		this.nsfwModel = model;
