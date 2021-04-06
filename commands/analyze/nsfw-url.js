dc9e708e32ef2ddd175c85b4414cd7251be83d93
Dragon Fire <danielbodendahl@gmail.com>
2021-04-05 21:56:08 -0400

    NSFW URL Command

---
+++ commands/analyze/nsfw-url.js
@@ -0,0 +1,28 @@
+const Command = require('../../structures/Command');
+const { isUrlNSFW } = require('../../util/Util');
+
+module.exports = class NsfwUrlCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'nsfw-url',
+			aliases: ['nsfw-uri', 'url-nsfw', 'uri-nsfw'],
+			group: 'analyze',
+			memberName: 'nsfw-url',
+			description: 'Determines if a URL is NSFW.',
+			args: [
+				{
+					key: 'url',
+					prompt: 'What URL would you like to test?',
+					type: 'url'
+				}
+			]
+		});
+	}
+
+	async run(msg, { url }) {
+		const nsfw = await isUrlNSFW(url, this.client.adultSiteList);
+		if (nsfw === null) return msg.reply('❔ This site sent an error, or just didn\'t respond.');
+		if (!nsfw) return msg.reply('👍 This site is safe!');
+		return msg.reply('👎 This site is NSFW.');
+	}
+};
