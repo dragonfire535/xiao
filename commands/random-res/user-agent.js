ea892b504f607deca448d6e9a6da2f62febd3967
Dragon Fire <danielbodendahl@gmail.com>
2021-04-18 20:52:20 -0400

    User Agent Command

---
+++ commands/random-res/user-agent.js
@@ -0,0 +1,17 @@
+const Command = require('../../structures/Command');
+const UserAgent = require('user-agents');
+
+module.exports = class UserAgentCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'user-agent',
+			group: 'random-res',
+			memberName: 'user-agent',
+			description: 'Responds with a random User Agent.'
+		});
+	}
+
+	run(msg) {
+		return msg.reply(new UserAgent().toString());
+	}
+};
