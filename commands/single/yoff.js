ae1d47a1098c32867cf0f958f7385abacc08cb06
Dragon Fire <danielbodendahl@gmail.com>
2021-04-23 11:27:19 -0400

    Add more yoffs

--- commands/single/yoff.js
+++ commands/single/yoff.js
@@ -1,18 +1,37 @@
 const Command = require('../../structures/Command');
 const path = require('path');
+const { list } = require('../../util/Util');
+const types = ['default', 'blastyoff', 'disyoffjs', 'yoffcirius', 'yoffice', 'yofficer'];
 
-module.exports = class SourceCommand extends Command {
+module.exports = class YoffCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'yoff',
 			group: 'single',
 			memberName: 'yoff',
 			description: 'Posts a picture that truly defines modern art.',
-			clientPermissions: ['ATTACH_FILES']
+			clientPermissions: ['ATTACH_FILES'],
+			credit: [
+				{
+					name: '1Computer1',
+					url: 'https://github.com/1Computer1',
+					reason: 'Images'
+				}
+			],
+			args: [
+				{
+					key: 'type',
+					prompt: `What type of yoff do you want to see? Either ${list(types, 'or')}.`,
+					type: 'string',
+					default: 'default',
+					oneOf: types,
+					parse: type => type.toLowerCase()
+				}
+			]
 		});
 	}
 
-	run(msg) {
-		return msg.say({ files: [path.join(__dirname, '..', '..', 'assets', 'images', 'yoff.png')] });
+	run(msg, { type }) {
+		return msg.say({ files: [path.join(__dirname, '..', '..', 'assets', 'images', 'yoff', `${type}.png`)] });
 	}
 };
