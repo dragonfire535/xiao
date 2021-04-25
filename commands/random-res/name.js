5dc59c2f8454ff70a8343721d2ddccfedb8d074f
Dragon Fire <danielbodendahl@gmail.com>
2021-04-25 09:49:04 -0400

    Use name api in name command

--- commands/random-res/name.js
+++ commands/random-res/name.js
@@ -1,7 +1,6 @@
 const Command = require('../../structures/Command');
+const request = require('node-superfetch');
 const { list } = require('../../util/Util');
-const names = require('../../assets/json/name');
-const all = [].concat(names.male, names.female);
 const genders = ['male', 'female', 'both'];
 
 module.exports = class NameCommand extends Command {
@@ -11,6 +10,14 @@ module.exports = class NameCommand extends Command {
 			group: 'random-res',
 			memberName: 'name',
 			description: 'Responds with a random name, with the gender of your choice.',
+			credit: [
+				{
+					name: 'Random User Generator',
+					url: 'https://randomuser.me/',
+					reason: 'API',
+					reasonURL: 'https://randomuser.me/documentation'
+				}
+			],
 			args: [
 				{
 					key: 'gender',
@@ -24,9 +31,20 @@ module.exports = class NameCommand extends Command {
 		});
 	}
 
-	run(msg, { gender }) {
-		const lastName = names.last[Math.floor(Math.random() * names.last.length)];
-		if (gender === 'both') return msg.say(`${all[Math.floor(Math.random() * all.length)]} ${lastName}`);
-		return msg.say(`${names[gender][Math.floor(Math.random() * names[gender].length)]} ${lastName}`);
+	async run(msg, { gender }) {
+		try {
+			const { body } = await request
+				.get('https://randomuser.me/api/')
+				.query({
+					inc: 'name',
+					noinfo: '',
+					gender: gender === 'both' ? '' : gender,
+					nat: 'AU,US,CA,GB'
+				});
+			const data = body.results[0].name;
+			return msg.say(`${data.first} ${data.last}`);
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
 	}
 };
