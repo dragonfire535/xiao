f0b5e004e0a0556fbd0c7b0465936f5f6c2740e8
Dragon Fire <danielbodendahl@gmail.com>
2021-05-03 17:18:18 -0400

    Lock down more in math

--- commands/edit-number/math.js
+++ commands/edit-number/math.js
@@ -1,6 +1,14 @@
 const Command = require('../../structures/Command');
 const core = require('mathjs/number');
 const math = core.create(core.all);
+math.import({
+	import: function () { throw new Error('Function import is disabled') },
+	createUnit: function () { throw new Error('Function createUnit is disabled') },
+	evaluate: function () { throw new Error('Function evaluate is disabled') },
+	parse: function () { throw new Error('Function parse is disabled') },
+	simplify: function () { throw new Error('Function simplify is disabled') },
+	derivative: function () { throw new Error('Function derivative is disabled') }
+  }, { override: true });
 
 module.exports = class MathCommand extends Command {
 	constructor(client) {
