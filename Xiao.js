fa6efa044366ec4f662f0cf8b0ecb5710f967af3
Dragon Fire <danielbodendahl@gmail.com>
2021-08-02 18:27:11 -0400

    Fix

--- Xiao.js
+++ Xiao.js
@@ -14,7 +14,19 @@ const client = new Client({
 		repliedUser: true
 	},
 	partials: ['GUILD_MEMBER', 'CHANNEL'],
-	intents: [Intents.NON_PRIVILEGED, Intents.FLAGS.GUILD_MEMBERS]
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+		Intents.FLAGS.GUILD_VOICE_STATES,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.GUILD_MESSAGE_TYPING,
+		Intents.FLAGS.GUILD_WEBHOOKS,
+		Intents.FLAGS.DIRECT_MESSAGES,
+		Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+		Intents.FLAGS.DIRECT_MESSAGE_TYPING
+	]
 });
 const { formatNumber, checkFileExists } = require('./util/Util');
 
