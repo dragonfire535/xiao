84712c2091f2378cd38f9d99b7fa70055084a6df
Dragon Fire <danielbodendahl@gmail.com>
2021-08-02 18:18:39 -0400

    Fix?

--- framework/Extensions.js
+++ framework/Extensions.js
@@ -1,22 +1,17 @@
-const { Structures, APIMessage } = require('discord.js');
+const { Message, APIMessage } = require('discord.js');
 
-module.exports = Structures.extend('Message', Message => {
-	class CommandMessage extends Message {
-		say(content, options) {
-			return this.channel.send(content, options);
-		}
+Reflect.defineProperty(Message.prototype, 'say', { value: function (content, options) {
+	return this.channel.send(content, options);
+} });
 
-		embed(embed, options) {
-			return this.channel.send(new APIMessage(this.channel, { embed, ...options }));
-		}
+Reflect.defineProperty(Message.prototype, 'embed', { value: function (embed, options) {
+	return this.channel.send(new APIMessage(this.channel, { embed, ...options }));
+} });
 
-		code(lang, content, options) {
-			return this.channel.send(content, { code: lang, ...options });
-		}
+Reflect.defineProperty(Message.prototype, 'code', { value: function (lang, content, options) {
+	return this.channel.send(content, { code: lang, ...options });
+} });
 
-		direct(content, options) {
-			return this.author.send(content, options);
-		}
-	}
-	return CommandMessage;
-});
+Reflect.defineProperty(Message.prototype, 'direct', { value: function (content, options) {
+	return this.author.send(content, options);
+} });
