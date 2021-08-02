41a96af2956466bd18e1e2e7762681585484c97b
Dragon Fire <danielbodendahl@gmail.com>
2021-08-02 19:59:05 -0400

    Fix

--- framework/Extensions.js
+++ framework/Extensions.js
@@ -1,7 +1,7 @@
 const { Message, MessagePayload, APIMessage } = require('discord.js');
 
 Reflect.defineProperty(Message.prototype, 'say', { value: function (content, options) {
-	return this.channel.send({ content, ...options });
+	return this.channel.send({ content: content || null, ...options });
 } });
 
 Reflect.defineProperty(Message.prototype, 'embed', { value: function (embed, options) {
@@ -9,9 +9,9 @@ Reflect.defineProperty(Message.prototype, 'embed', { value: function (embed, opt
 } });
 
 Reflect.defineProperty(Message.prototype, 'code', { value: function (lang, content, options) {
-	return this.channel.send({ content, lang, ...options });
+	return this.channel.send({ content: content || null, lang, ...options });
 } });
 
 Reflect.defineProperty(Message.prototype, 'direct', { value: function (content, options) {
-	return this.author.send({ content, options });
+	return this.author.send({ content: content || null, options });
 } });
