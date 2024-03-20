48629a9515bce38693ea443cc094e5800d6a4c4b
Dragon Fire <danielbodendahl@gmail.com>
2024-03-20 01:22:00 -0400

    Fix

--- commands/util-voice/pause.js
+++ commands/util-voice/pause.js
@@ -17,9 +17,6 @@ module.exports = class PauseCommand extends Command {
 	run(msg) {
 		const connection = getVoiceConnection(msg.guild.id);
 		if (!connection) return msg.reply('I am not in a voice channel.');
-		if (!msg.channel.permissionsFor(msg.author).has('MOVE_MEMBERS') && connection.channel.members.size > 2) {
-			return msg.reply('You need the "Move members" permission to stop playing audio.');
-		}
 		if (!this.client.dispatchers.has(msg.guild.id)) {
 			return msg.reply(`I am not currently playing audio in this server.`);
 		}
