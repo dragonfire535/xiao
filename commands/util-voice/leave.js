bf0514c73cafa33e027ba7a4715ab7d78f3d1c56
Dragon Fire <danielbodendahl@gmail.com>
2024-03-20 01:22:00 -0400

    Fix

--- commands/util-voice/leave.js
+++ commands/util-voice/leave.js
@@ -17,12 +17,6 @@ module.exports = class LeaveCommand extends Command {
 	run(msg) {
 		const connection = getVoiceConnection(msg.guild.id);
 		if (!connection) return msg.reply('I am not in a voice channel.');
-		if (!connection.channel.members.has(msg.author.id)) {
-			return msg.reply('You must be in the voice channel to remove me from it.');
-		}
-		if (!msg.channel.permissionsFor(msg.author).has('MOVE_MEMBERS') && connection.channel.members.size > 2) {
-			return msg.reply('You need the "Move members" permission to remove me from this voice channel.');
-		}
 		if (this.client.dispatchers.has(msg.guild.id)) {
 			const usage = this.client.registry.commands.get('stop').usage();
 			return msg.reply(`I am currently playing audio in this server. Please use ${usage} first.`);
