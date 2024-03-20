9d135b98188e29b47062f9d0090eb7953269881c
Dragon Fire <danielbodendahl@gmail.com>
2024-03-19 22:58:31 -0400

    Fix info invite

--- commands/util-public/info.js
+++ commands/util-public/info.js
@@ -5,7 +5,6 @@ require('moment-duration-format');
 const { formatNumber, embedURL } = require('../../util/Util');
 const { version, dependencies, optionalDependencies } = require('../../package');
 const deps = { ...dependencies, ...optionalDependencies };
-// const permissions = require('../../assets/json/permissions');
 const copyright = require('../../assets/json/copyright');
 
 module.exports = class InfoCommand extends Command {
@@ -22,8 +21,7 @@ module.exports = class InfoCommand extends Command {
 	}
 
 	async run(msg) {
-		// const invite = await this.client.generateInvite({ permissions, scopes: ['bot'] });
-		const invite = 'https://google.com';
+		const invite = this.client.generateInvite({ permissions: 8, scopes: ['bot'] });
 		const embed = new MessageEmbed()
 			.setColor(0x00AE86)
 			.setFooter(copyright.join('\n'))
