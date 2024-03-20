909eb3a0cc793dceaa765767a37fb98417150b71
Dragon Fire <danielbodendahl@gmail.com>
2024-03-20 18:53:25 -0400

    Combine percent-diff and text-diff

--- commands/analyze/text-diff.js
+++ commands/analyze/text-diff.js
@@ -1,11 +1,12 @@
 const Command = require('../../framework/Command');
 const Diff = require('text-diff');
+const wuzzy = require('wuzzy');
 
 module.exports = class TextDiffCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'text-diff',
-			aliases: ['diff'],
+			aliases: ['diff', 'percent-diff', 'name-diff'],
 			group: 'analyze',
 			memberName: 'text-diff',
 			description: 'Compares two different bits of text.',
@@ -27,6 +28,7 @@ module.exports = class TextDiffCommand extends Command {
 	run(msg, { text1, text2 }) {
 		const diff = new Diff();
 		const textDiff = diff.main(text1, text2);
+		const wuzzyDiff = wuzzy.jaccard(text1.toLowerCase(), text2.toLowerCase());
 		diff.cleanupSemantic(textDiff);
 		const formatted = textDiff.map(change => {
 			if (change[0] === 1) return `**${change[1]}**`;
@@ -34,6 +36,6 @@ module.exports = class TextDiffCommand extends Command {
 			if (change[0] === -1) return `~~${change[1]}~~`;
 			return '';
 		}).join('');
-		return msg.reply(formatted);
+		return msg.reply(`${formatted} (${Math.round(diff * 100)}% similarity)`);
 	}
 };
