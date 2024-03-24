09cb856af19365efee0cf881b25d9cf7c1281c08
Dragon Fire <danielbodendahl@gmail.com>
2024-03-23 23:57:14 -0400

    Fix default emoji type

--- framework/types/default-emoji.js
+++ framework/types/default-emoji.js
@@ -1,18 +1,17 @@
 const ArgumentType = require('../ArgumentType');
-const emojiRegex = require('emoji-regex');
+const emojiRegex = require('emoji-regex')();
 
 module.exports = class DefaultEmojiArgumentType extends ArgumentType {
 	constructor(client) {
 		super(client, 'default-emoji');
-		this.regex = new RegExp(`^(?:${emojiRegex().toString()})$`);
 	}
 
 	validate(value) {
-		if (!this.regex.test(value)) return false;
+		if (!emojiRegex.test(value)) return false;
 		return true;
 	}
 
 	parse(value) {
-		return value;
+		return value.match(emojiRegex)[0];
 	}
 };
