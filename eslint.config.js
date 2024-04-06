c055419ce17780a4b085a45e53e17c7831d3ac8c
Dragon Fire <danielbodendahl@gmail.com>
2024-04-06 17:58:44 -0400

    Why

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,16 @@
 const { default: eslintPluginJsonc } = require('eslint-plugin-jsonc');
 const js = require('@eslint/js');
 const amber = require('eslint-config-amber');
+const globals = require('globals');
 
 module.exports = [
 	...eslintPluginJsonc.configs['flat/recommended-with-json'],
 	{
 		files: ['**/*.js'],
-		languageOptions: { sourceType: 'commonjs' },
+		languageOptions: {
+			sourceType: 'commonjs',
+			globals: { ...globals.node }
+		},
 		rules: {
 			...js.configs.recommended.rules,
 			...amber.rules
