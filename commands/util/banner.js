58c81aa912bd8061ddf5f382808aa84e12a6af3d
Dragon Fire <danielbodendahl@gmail.com>
2024-04-07 17:12:19 -0400

    Fix

--- commands/util/banner.js
+++ commands/util/banner.js
@@ -1,6 +1,7 @@
 const Command = require('../../framework/Command');
 const { DataResolver } = require('discord.js');
-const { REST, Routes } = require('@discordjs/rest');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
 const { XIAO_TOKEN } = process.env;
 const rest = new REST({ version: '10' }).setToken(XIAO_TOKEN);
 
