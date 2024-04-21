09de3ed521f1e11f9cf3a0562874ef6d91f6a4c9
Dragon Fire <danielbodendahl@gmail.com>
2024-04-21 15:06:15 -0400

    Add Twemoji font

--- structures/Font.js
+++ structures/Font.js
@@ -11,7 +11,7 @@ const weights = {
 	900: 'heavy',
 	950: 'extraBlack'
 };
-const fallbacks = ['Symbola', 'Noto-CJK'];
+const fallbacks = ['TwitterColorEmoji', 'Symbola', 'Noto-CJK'];
 
 module.exports = class Font {
 	constructor(path, filename, metadata) {
