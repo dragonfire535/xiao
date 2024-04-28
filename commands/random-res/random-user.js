88a1d7df68c75a392e739997816e5636485d49a8
Dragon Fire <danielbodendahl@gmail.com>
2024-04-28 18:29:56 -0400

    Make special reply for Xiao in random-user

--- commands/random-res/random-user.js
+++ commands/random-res/random-user.js
@@ -12,10 +12,14 @@ module.exports = class RandomUserCommand extends Command {
 	}
 
 	run(msg) {
-		if (!msg.guild) {
+		let member;
+		if (msg.guild) {
+			member = msg.guild.members.cache.random().user;
+		} else {
 			const members = [this.client.user, msg.channel.recipient];
-			return msg.say(`I choose ${members[Math.floor(Math.random() * members.length)].tag}!`);
+			member = members[Math.floor(Math.random() * members.length)];
 		}
-		return msg.say(`I choose ${msg.guild.members.cache.random().user.tag}!`);
+		if (member.id === this.client.user.id) return msg.say('I choose myself!');
+		return msg.say(`I choose ${member.tag}!`);
 	}
 };
