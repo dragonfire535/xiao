ad6b2b27beebf9912d6d6943ef3a456febc608a2
Dragon Fire <danielbodendahl@gmail.com>
2024-05-09 16:07:01 -0400

    Fix

--- commands/edit-image-text/tweet.js
+++ commands/edit-image-text/tweet.js
@@ -202,6 +202,7 @@ module.exports = class TweetCommand extends Command {
 			ctx.clip();
 		}
 		ctx.drawImage(avatar, 17, 84, 52, 52);
+		canvas.height -= 1;
 		return msg.say({ files: [{ attachment: canvas.toBuffer('image/png'), name: 'tweet.png' }] });
 	}
 
