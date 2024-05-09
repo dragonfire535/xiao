f2147f3d85e2321587d9f2df436ccf4c06d9879e
Dragon Fire <danielbodendahl@gmail.com>
2024-05-09 15:14:04 -0400

    Fix

--- structures/Tensorflow.js
+++ structures/Tensorflow.js
@@ -74,10 +74,10 @@ module.exports = class Tensorflow {
 	async stylizeImage(image, styleImg) {
 		const imageTensor = await tf.node.decodeImage(image, 3);
 		const [originalHeight, originalWidth] = imageTensor.shape.slice(0, 2);
-		const desiredWidth = 400;
+		const desiredHeight = 400;
 		const aspectRatio = originalWidth / originalHeight;
-		const newHeight = Math.round(desiredWidth / aspectRatio);
-		const resizedImage = tf.image.resizeBilinear(imageTensor, [newHeight, desiredWidth]);
+		const newWidth = Math.round(desiredHeight / aspectRatio);
+		const resizedImage = tf.image.resizeBilinear(imageTensor, [desiredHeight, newWidth]);
 		imageTensor.dispose();
 		const loadedImage = resizedImage.toFloat().div(tf.scalar(255)).expandDims();
 		resizedImage.dispose();
